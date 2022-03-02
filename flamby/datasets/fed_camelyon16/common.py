131ad2426852bb0eea26ae6f5ee76473c879b865
jeandut <jean.du-terrail@owkin.com>
2022-03-02 14:40:16 +0100

    adding dataset code

---
+++ flamby/datasets/fed_camelyon16/common.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+METADATA_DICT = {"tiles_dir": "", "labels": pd.DataFrame()}
+NUM_CLIENTS = 2
+NB_MAX_ROUNDS = 20
---
+++ flamby/datasets/fed_camelyon16/dataset.py
@@ -0,0 +1,208 @@
+
+import os
+from pathlib import Path
+
+import numpy as np
+import pandas as pd
+import torch
+from  flamby.datasets.fed_camelyon16 import METADATA_DICT
+from torch.utils.data import Dataset
+
+dic = METADATA_DICT
+
+class Camelyon16Raw(Dataset):
+    """Pytorch dataset containing all the features, labels and
+    metadata for Camelyon16 WS without any discrimination.
+    Attributes
+    ----------
+    tiles_dir : str, Where all features are located extracted from hogrc
+    labels: pd.DataFrame, The ground truth DataFrame for labels
+    metadata: pd.DataFrame, The ground truth dataframe for metadata such as centers
+    features_paths: list[str], The list with the path towards all features.
+    features_labels: list[int], The list with all classification labels for all features
+    features_centers: list[int], The list for all centers for all features
+    features_sets: list[str], The list for all sets (train/test) for all features
+    X_dtype: torch.dtype, The dtype of the X features output
+    y_dtype: torch.dtype, The dtype of the y label output
+    """
+
+    def __init__(
+        self, X_dtype=torch.float32, y_dtype=torch.int64, remove_spatial_info=True
+    ):
+        """See description above
+        Parameters
+        ----------
+        X_dtype : torch.dtype, optional
+            Dtype for inputs `X`. Defaults to `torch.float32`.
+        y_dtype : torch.dtype, optional
+            Dtype for labels `y`. Defaults to `torch.int64`.
+        remove_spatial_info : bool, optional
+            Whether to remove spatial information or not. Defaults to True.
+        """
+        self.tiles_dir = Path(dic["tiles_dir"])
+        self.labels = pd.read_csv(dic["labels"], index_col="filenames")
+        self.metadata = pd.read_csv(
+            Path(ROOT_PACKAGE_PATH)
+            / Path("metadata")
+            / Path("data_camelyon16_metadata_camelyon.csv")
+        )
+        self.X_dtype = X_dtype
+        self.y_dtype = y_dtype
+        self.remove_spatial_info = True
+        self.features_paths = []
+        self.features_labels = []
+        self.features_centers = []
+        self.features_sets = []
+        for slide in self.tiles_dir.glob("*.npy"):
+            slide_name = os.path.basename(slide).split(".")[0]
+            slide_id = int(slide_name.split("_")[1])
+
+            label_from_metadata = int(
+                self.metadata.loc[
+                    [
+                        os.path.basename(e).split(".")[0] == slide_name
+                        for e in self.metadata["path2slide"].tolist()
+                    ],
+                    "label",
+                ].item()
+            )
+            center_from_metadata = int(
+                self.metadata.loc[
+                    [
+                        os.path.basename(e).split(".")[0] == slide_name
+                        for e in self.metadata["path2slide"].tolist()
+                    ],
+                    "hospital_corrected",
+                ].item()
+            )
+            label_from_data = int(self.labels.loc[slide.name].tumor)
+
+            if "Test" not in str(slide):
+                if slide_name.startswith("Normal"):
+                    # Normal slide
+                    if slide_id > 100:
+                        center_label = 1
+
+                    else:
+                        center_label = 0
+                    label_from_slide_name = 0  # Normal slide
+                elif slide_name.startswith("tumor") or slide_name.startswith("Tumor"):
+                    # Tumor slide
+                    if slide_id > 70:
+                        center_label = 1
+                    else:
+                        center_label = 0
+                    label_from_slide_name = 1  # Tumor slide
+                self.features_sets.append("train")
+                assert label_from_slide_name == label_from_data, "This shouldn't happen"
+                assert center_label == center_from_metadata, "This shouldn't happen"
+
+            else:
+                self.features_sets.append("test")
+
+            assert label_from_metadata == label_from_data
+            self.features_paths.append(slide)
+            self.features_labels.append(label_from_data)
+            self.features_centers.append(center_from_metadata)
+
+    def __len__(self):
+        return len(self.features_paths)
+
+    def __getitem__(self, idx):
+        start = 3 if self.remove_spatial_info else 0
+        X = np.load(self.features_paths[idx])[:, start:]
+        X = torch.from_numpy(X).to(self.X_dtype)
+        y = torch.from_numpy(np.asarray(self.features_labels[idx])).to(self.y_dtype)
+        return X, y
+
+
+class FedCamelyon16(Camelyon16Raw):
+    """
+    Pytorch dataset containing for each center the features and associated labels
+    for Camelyon16 federated classification.
+    One can instantiate this dataset with train or test data coming from either
+    of the 2 centers it was created from or all data pooled.
+    The train/test split corresponds to the one from the Challenge.
+    """
+
+    def __init__(
+        self,
+        center=0,
+        train=True,
+        pooled=False,
+        X_dtype=torch.float32,
+        y_dtype=torch.int64,
+        remove_spatial_info=True,
+    ):
+        """Instantiate the dataset
+        Parameters
+        pooled : bool, optional
+            Whether to take all data from the 2 centers into one dataset, by
+            default False
+        X_dtype : torch.dtype, optional
+            Dtype for inputs `X`. Defaults to `torch.float32`.
+        y_dtype : torch.dtype, optional
+            Dtype for labels `y`. Defaults to `torch.int64`.
+        remove_spatial_info : bool, optional
+            Whether to remove spatial information or not. Defaults to True.
+        """
+        super().__init__(
+            X_dtype=X_dtype, y_dtype=y_dtype, remove_spatial_info=remove_spatial_info
+        )
+        assert center in [0, 1]
+        self.centers = [center]
+        if pooled:
+            self.centers = [0, 1]
+        if train:
+            self.sets = ["train"]
+        else:
+            self.sets = ["test"]
+
+        to_select = [
+            (self.features_sets[idx] in self.sets)
+            and (self.features_centers[idx] in self.centers)
+            for idx, _ in enumerate(self.features_centers)
+        ]
+        self.features_paths = [
+            fp for idx, fp in enumerate(self.features_paths) if to_select[idx]
+        ]
+        self.features_sets = [
+            fp for idx, fp in enumerate(self.features_sets) if to_select[idx]
+        ]
+        self.features_labels = [
+            fp for idx, fp in enumerate(self.features_labels) if to_select[idx]
+        ]
+        self.features_centers = [
+            fp for idx, fp in enumerate(self.features_centers) if to_select[idx]
+        ]
+
+
+def collate_fn(dataset_elements_list, max_tiles=10000):
+    """Helper function to correctly batch samples from
+    a Camelyon16Dataset accomodating for the uneven number of tiles per slide.
+
+    Parameters
+    ----------
+    dataset_elements_list : List[torch.Tensor]
+        A list of torch tensors of dimensions [n, 2048] with uneven distribution of ns.
+    max_tiles : int, optional
+        The nummber of tiles max by Tensor, by default 10000
+
+    Returns
+    -------
+    Tuple(torch.Tensor, torch.Tensor)
+        X, y two torch tensors of size (len(dataset_elements_list), max_tiles, 2048) and (len(dataset_elements_list),)
+    """
+    n = len(dataset_elements_list)
+    X0, y0 = dataset_elements_list[0]
+    X_dtype = X0.dtype
+    y_dtype = y0.dtype
+    X = torch.zeros((n, max_tiles, 2048), dtype=X_dtype)
+    y = torch.empty((n,), dtype=y_dtype)
+
+    for i in range(n):
+        X_current, y_current = dataset_elements_list[i]
+        ntiles_min = min(max_tiles, X_current.shape[0])
+        X[i, :ntiles_min, :] = X_current[:ntiles_min, :]
+        y[i] = y_current
+    return X, y
