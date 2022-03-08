840235b9ae0578edc18b9d4d79ba139dc8f436b5
jeandut <jean.du-terrail@owkin.com>
2022-03-08 22:32:46 +0100

    removing comment and triggering linting

--- flamby/datasets/fed_camelyon16/dataset_creation_scripts/tiling_slides.py
+++ flamby/datasets/fed_camelyon16/dataset_creation_scripts/tiling_slides.py
@@ -1,17 +1,17 @@
-from glob import glob
 import argparse
 import os
-import io
-import torch
 import tempfile
+from glob import glob
+
 import numpy as np
+import torch
+import torchvision.models as models
 from histolab.slide import Slide
 from histolab.tiler import GridTiler
-from torch.utils.data import Dataset, DataLoader
-import torchvision.models as models
-from torch.nn import Identity
-from torchvision.transforms import Compose, ToTensor, Normalize
 from skimage import io
+from torch.nn import Identity
+from torch.utils.data import DataLoader, Dataset
+from torchvision.transforms import Compose, Normalize, ToTensor
 from tqdm import tqdm
 
 
@@ -33,6 +33,7 @@ class ImageDataset(Dataset):
             sample = self.transform(sample)
         return sample
 
+
 def main(slides_dir, features_dir, batch_size):
     """This function tile the matter on the slides located into slides_dir using
     a resnet50 pretrained on IMAGENET with a batch size of batch_size for inference.
@@ -45,19 +46,19 @@ def main(slides_dir, features_dir, batch_size):
     features_dir : str
         The folder where to store the produced features.
     batch_size : int
-        The batch size for the resnet50's inference, depends on the capacity of the distant machine.
+        The batch size for the resnet50's inference, depends on the capacity of
+        the distant machine.
     """
     os.makedirs(features_dir, exist_ok=True)
     slides_paths = glob(os.path.join(slides_dir, "*.tif"))
-    #assert slides_paths 
     grid_tiles_extractor = GridTiler(
         tile_size=(256, 256),
         level=0,
-        pixel_overlap=0, 
+        pixel_overlap=0,
         prefix="grid/",
         check_tissue=True,
         suffix=".png",
-        )
+    )
     net = models.resnet50(pretrained=True)
     # Probably unnecessary still it feels safer
     for param in net.parameters():
@@ -67,27 +68,35 @@ def main(slides_dir, features_dir, batch_size):
         net = net.cuda()
     net.fc = Identity()
     # IMAGENET preprocessing of images scaled between 0. and 1. with ToTensor
-    transform = Compose([ToTensor(), Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])
+    transform = Compose(
+        [ToTensor(), Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])]
+    )
     # Tiling all downloaded slides with provided model
     for sp in tqdm(slides_paths):
-        path_to_feature = os.path.join(features_dir, slide_name + ".npy")
+        slide_name = os.path.basename(sp)
+        path_to_features = os.path.join(features_dir, slide_name + ".npy")
         if os.path.exists(path_to_features):
             continue
-        slide_name = os.path.basename(sp)
         with tempfile.TemporaryDirectory() as tmpdirname:
             slide = Slide(sp, tmpdirname, use_largeimage=True)
             grid_tiles_extractor.extract(slide)
             images_list = glob(os.path.join(tmpdirname, "**", "*.png"))
             dataset_from_slide = ImageDataset(images_list, transform=transform)
-            dataloader = DataLoader(dataset_from_slide, batch_size=batch_size, pin_memory=torch.cuda.is_available())
+            dataloader = DataLoader(
+                dataset_from_slide,
+                batch_size=batch_size,
+                pin_memory=torch.cuda.is_available(),
+            )
             with torch.inference_mode():
                 features = np.zeros((len(images_list), 2048)).astype("float32")
                 for i, batch in enumerate(iter(dataloader)):
                     if torch.cuda.is_available():
                         batch = batch.cuda()
-                    features[(i*batch_size):((i+1)*batch_size)] = net(batch).detach().cpu().numpy()
+                    start_idx = i * batch_size
+                    end_idx = (i + 1) * batch_size
+                    features[start_idx:end_idx] = net(batch).detach().cpu().numpy()
         np.save(path_to_features, features)
-                
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -112,8 +121,4 @@ if __name__ == "__main__":
     )
 
     args = parser.parse_args()
-    main(
-        args.slides_dir,
-        args.output_folder,
-        args.batch_size,
-    )
+    main(args.slides_dir, args.output_folder, args.batch_size)
