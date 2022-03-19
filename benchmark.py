fee0e399179d41c3487dba96093714d0d1b8525d
jeandut <jean.du-terrail@owkin.com>
2022-03-20 00:30:09 +0100

    adding small training script alongside metrics and losses

---
+++ benchmark.py
@@ -0,0 +1,39 @@
+from flamby.datasets.fed_camelyon16 import FedCamelyon16, Baseline, BaselineLoss, metric, collate_fn
+from torch.utils.data import DataLoader as dl
+import torch.optim as optim
+from flamby.utils import evaluate_model_on_tests
+
+
+BATCH_SIZE = 64
+NUM_WORKERS_TORCH = 10
+NUM_EPOCHS = 30
+
+training_dl = dl(FedCamelyon16(train=True, pooled=True), num_workers=NUM_WORKERS_TORCH, batch_size=BATCH_SIZE, collate_fn=collate_fn, shuffle=True)
+test_dl = dl(FedCamelyon16(train=False, pooled=True), num_workers=NUM_WORKERS_TORCH, batch_size=BATCH_SIZE, collate_fn=collate_fn, shuffle=False)
+
+m = Baseline()
+loss = BaselineLoss()
+optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+
+for e in range(NUM_EPOCHS):
+    for X, y in training_dl:
+        if torch.cuda.is_available():
+            X = X.cuda()
+            y = y.cuda()
+        optimizer.zero_grad()
+        y_pred = m(X)
+        l = loss(y_pred, y)
+        l.backward()
+        otimizer.step()
+
+evaluate_model_on_tests(m, [test_dl], metric)
+
+
+        
+
+
+
+
+
+
+
---
+++ flamby/datasets/fed_camelyon16/loss.py
@@ -0,0 +1,12 @@
+from torch.nn.modules.loss import _Loss
+import torch
+
+
+class BaselineLoss(_Loss):
+    def __init__(self, reduction="mean"):
+        super(BaselineLoss, self).__init__(reduction=reduction)
+
+    def forward(self, input: torch.Tensor, target: torch.Tensor):
+        y_pred = input[0]
+        bce = torch.nn.BCEWithLogitsLoss()
+        return bce(y_pred, target)
