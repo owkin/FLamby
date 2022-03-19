bcfed983b5de75e5ca284bd5744f249fb391d25a
jeandut <jean.du-terrail@owkin.com>
2022-03-20 00:30:09 +0100

    adding small training script alongside metrics and losses

--- flamby/datasets/fed_camelyon16/__init__.py
+++ flamby/datasets/fed_camelyon16/__init__.py
@@ -1,3 +1,5 @@
 from .common import NUM_CLIENTS, NB_MAX_ROUNDS
-from .dataset import Camelyon16Raw, FedCamelyon16
+from .dataset import Camelyon16Raw, FedCamelyon16, collate_fn
 from .model import Baseline
+from .metric import metric
+from .loss import BaselineLoss
