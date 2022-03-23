e8f36e1f3e50215e1fe7afec31881f94a80615e9
Boris MUZELLEC <boris.muzellec@owkin.com>
2022-03-23 14:43:19 +0100

    random patch sampling

--- flamby/datasets/fed_lidc_idri/data_utils.py
+++ flamby/datasets/fed_lidc_idri/data_utils.py
@@ -43,3 +43,47 @@ def resize_by_crop_or_pad(X, output_shape=(384, 384, 384)):
         X = X.narrow(d, start, output_shape[d])
 
     return X
+
+
+def random_sampler(image, label, patch_shape=(48, 48, 48), n_samples=2):
+    """
+    Sample random patches from input of any dimension
+    Parameters
+    ----------
+    image : torch.Tensor
+        input image tensor
+    label : torch.Tensor
+        label map
+    patch_shape : int Tuple
+        desired shape of patches
+    n_samples : int, optional
+        number of output patches
+    Returns
+    -------
+    image_patches : torch.Tensor
+        random image patches
+    label_patches : torch.Tensor
+        random label patches (at same positions as images)
+    """
+    # Assumes one channel for the image
+    maxvals = np.maximum(np.subtract(image.shape, patch_shape), 1)
+
+    begins = (torch.rand((n_samples, image.dim())) * np.max(maxvals)).long() % maxvals[
+        None, ...
+    ]
+
+    begins = begins[:, None, None, None, :]
+
+    ranges = [torch.arange(s) for s in patch_shape]
+
+    # Build the tensor of indices for further slicing
+    indices = (
+        begins + torch.stack(torch.meshgrid(*ranges, indexing="ij"), dim=-1)[None, ...]
+    )
+
+    # Dirty hack to mimic tf.gather_nd
+    indices_list = [indices[..., i] for i in range(indices.shape[-1])]
+    image_patches = image[indices_list]
+    label_patches = label[indices_list]
+
+    return image_patches, label_patches
