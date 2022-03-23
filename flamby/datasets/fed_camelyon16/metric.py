652dcbcc508b04fbdef9c405bdcb1f5e0c8fa41c
jeandut <jean.du-terrail@owkin.com>
2022-03-23 18:00:45 +0100

    finished benchmark with 5 runs and tensorboard support

--- flamby/datasets/fed_camelyon16/metric.py
+++ flamby/datasets/fed_camelyon16/metric.py
@@ -1,6 +1,11 @@
 from sklearn.metrics import roc_auc_score
+import numpy as np
 
 
 def metric(y_true, y_pred):
     y_true = y_true.astype("uint8")
-    return roc_auc_score(y_true, y_pred)
+    # The try except is needed because when the metric is batch some batches have one class only
+    try:
+        return roc_auc_score(y_true, y_pred)
+    except:
+        return np.nan
