814e5fb1ce8859e3e4df24d00d8ba2b3975a13f3
jeandut <jean.du-terrail@owkin.com>
2022-03-23 18:12:38 +0100

    adding default arguments

--- flamby/datasets/fed_camelyon16/common.py
+++ flamby/datasets/fed_camelyon16/common.py
@@ -1,4 +1,10 @@
 import pandas as pd
 
 NUM_CLIENTS = 2
-NB_MAX_ROUNDS = 20
+BATCH_SIZE = 32
+NUM_EPOCHS_POOLED = 40
+LR = 0.001
+
+def get_nb_max_rounds(num_updates, batch_size=BATCH_SIZE):
+    # TODO replace 300 with the actual precise number
+    return (300 // BATCH_SIZE) * NUM_EPOCHS_POOLED // 50
