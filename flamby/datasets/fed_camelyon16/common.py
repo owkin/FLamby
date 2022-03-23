c4d1c352180e226faf217e50711def1fbdc0d4f5
jeandut <jean.du-terrail@owkin.com>
2022-03-23 19:02:44 +0100

    finishing wp2

--- flamby/datasets/fed_camelyon16/common.py
+++ flamby/datasets/fed_camelyon16/common.py
@@ -2,9 +2,8 @@ import pandas as pd
 
 NUM_CLIENTS = 2
 BATCH_SIZE = 32
-NUM_EPOCHS_POOLED = 40
+NUM_EPOCHS_POOLED = 100
 LR = 0.001
 
 def get_nb_max_rounds(num_updates, batch_size=BATCH_SIZE):
-    # TODO replace 300 with the actual precise number
-    return (300 // BATCH_SIZE) * NUM_EPOCHS_POOLED // 50
+    return (270 // BATCH_SIZE) * NUM_EPOCHS_POOLED // num_updates
