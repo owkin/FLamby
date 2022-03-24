621f9a246ec406b08a31298580fc5c886c5166be
jeandut <jean.du-terrail@owkin.com>
2022-03-24 09:37:12 +0100

    Linting python scripts

--- flamby/datasets/fed_camelyon16/benchmark.py
+++ flamby/datasets/fed_camelyon16/benchmark.py
@@ -1,11 +1,21 @@
-from flamby.datasets.fed_camelyon16 import FedCamelyon16, Baseline, BaselineLoss, metric, collate_fn, BATCH_SIZE, NUM_EPOCHS_POOLED, LR
-from torch.utils.data import DataLoader as dl
-import torch.optim as optim
-from flamby.utils import evaluate_model_on_tests
-import torch
-from tqdm import tqdm
 import numpy as np
+import torch
+import torch.optim as optim
+from torch.utils.data import DataLoader as dl
 from torch.utils.tensorboard import SummaryWriter
+from tqdm import tqdm
+
+from flamby.datasets.fed_camelyon16 import (
+    BATCH_SIZE,
+    LR,
+    NUM_EPOCHS_POOLED,
+    Baseline,
+    BaselineLoss,
+    FedCamelyon16,
+    collate_fn,
+    metric,
+)
+from flamby.utils import evaluate_model_on_tests
 
 torch.use_deterministic_algorithms(True)
 
@@ -13,25 +23,39 @@ torch.use_deterministic_algorithms(True)
 NUM_WORKERS_TORCH = 20
 DEBUG = False
 LOG = True
-LOG_PERIOD = 10 
+LOG_PERIOD = 10
 
 
 metrics_dict = {"AUC": metric}
-training_dl = dl(FedCamelyon16(train=True, pooled=True, debug=DEBUG), num_workers=NUM_WORKERS_TORCH, batch_size=BATCH_SIZE, collate_fn=collate_fn, shuffle=True)
-test_dl = dl(FedCamelyon16(train=False, pooled=True, debug=DEBUG), num_workers=NUM_WORKERS_TORCH, batch_size=BATCH_SIZE, collate_fn=collate_fn, shuffle=False)
+training_dl = dl(
+    FedCamelyon16(train=True, pooled=True, debug=DEBUG),
+    num_workers=NUM_WORKERS_TORCH,
+    batch_size=BATCH_SIZE,
+    collate_fn=collate_fn,
+    shuffle=True,
+)
+test_dl = dl(
+    FedCamelyon16(train=False, pooled=True, debug=DEBUG),
+    num_workers=NUM_WORKERS_TORCH,
+    batch_size=BATCH_SIZE,
+    collate_fn=collate_fn,
+    shuffle=False,
+)
 
 
 if LOG:
     # We compute the number of batches per epoch
-    num_local_steps_per_epoch = len(training_dl.dataset) // BATCH_SIZE 
-    num_local_steps_per_epoch += int((len(training_dl.dataset) - num_local_steps_per_epoch * BATCH_SIZE) > 0)
+    num_local_steps_per_epoch = len(training_dl.dataset) // BATCH_SIZE
+    num_local_steps_per_epoch += int(
+        (len(training_dl.dataset) - num_local_steps_per_epoch * BATCH_SIZE) > 0
+    )
 
 
 results = []
 for seed in range(42, 47):
     # At each new seed we re-initialize the model
-    # and training_dl is shuffled as well  
-    torch.manual_seed(seed) 
+    # and training_dl is shuffled as well
+    torch.manual_seed(seed)
     m = Baseline()
     # We put the model on GPU whenever it is possible
     if torch.cuda.is_available():
@@ -52,19 +76,27 @@ for seed in range(42, 47):
             if torch.cuda.is_available():
                 X = X.cuda()
                 y = y.cuda()
-    
+
             optimizer.zero_grad()
             y_pred = m(X)
-            l = loss(y_pred, y)
-            l.backward()
+            lm = loss(y_pred, y)
+            lm.backward()
             optimizer.step()
             if LOG:
                 current_step = s + num_local_steps_per_epoch * e
                 if (current_step % LOG_PERIOD) == 0:
-                    writer.add_scalar(f"Loss/train/client", l.item(), s + num_local_steps_per_epoch * e)
+                    writer.add_scalar(
+                        "Loss/train/client", lm.item(), s + num_local_steps_per_epoch * e
+                    )
                     for k, v in metrics_dict.items():
-                        train_batch_metric = v(y.detach().cpu().numpy(), y_pred.detach().cpu().numpy())
-                        writer.add_scalar(f"{k}/train/client", train_batch_metric, s + num_local_steps_per_epoch * e)
+                        train_batch_metric = v(
+                            y.detach().cpu().numpy(), y_pred.detach().cpu().numpy()
+                        )
+                        writer.add_scalar(
+                            f"{k}/train/client",
+                            train_batch_metric,
+                            s + num_local_steps_per_epoch * e,
+                        )
 
     current_results_dict = evaluate_model_on_tests(m, [test_dl], metric)
     print(current_results_dict)
@@ -73,10 +105,10 @@ for seed in range(42, 47):
 results = np.array(results)
 
 if LOG:
-    writer = SummaryWriter(log_dir=f"./runs/tests")
+    writer = SummaryWriter(log_dir="./runs/tests")
     for i in range(results.shape[0]):
-        writer.add_scalar(f"AUC/client_test_{i}", results[i], 0)
+        writer.add_scalar("AUC/client_test_{i}", results[i], 0)
 
 
 print("Benchmark Results on Camelyon16 pooled:")
-print(f"mAUC on 5 runs: {results.mean(): .2%} \pm {results.std(): .2%}")
+print(f"mAUC on 5 runs: {results.mean(): .2%} \\pm {results.std(): .2%}")
