6bddad5fd6a6f1cc62c5cf2aeea4be70df6e0d1c
jeandut <jean.du-terrail@owkin.com>
2022-03-24 11:47:34 +0100

    adding instructions and better tensorboarding

--- flamby/datasets/fed_camelyon16/benchmark.py
+++ flamby/datasets/fed_camelyon16/benchmark.py
@@ -63,7 +63,8 @@ def main(num_workers_torch, log=False, log_period=10, debug=False, cpu_only=Fals
         )
 
     results = []
-    for seed in range(42, 47):
+    seeds = np.arange(42, 47).tolist()
+    for seed in seeds:
         # At each new seed we re-initialize the model
         # and training_dl is shuffled as well
         torch.manual_seed(seed)
@@ -120,9 +121,9 @@ def main(num_workers_torch, log=False, log_period=10, debug=False, cpu_only=Fals
     results = np.array(results)
 
     if log:
-        writer = SummaryWriter(log_dir="./runs/tests")
         for i in range(results.shape[0]):
-            writer.add_scalar("AUC/client_test_{i}", results[i], 0)
+            writer = SummaryWriter(log_dir=f"./runs/tests_seed{seeds[i]}")
+            writer.add_scalar("AUC-test", results[i], 0)
 
     print("Benchmark Results on Camelyon16 pooled:")
     print(f"mAUC on 5 runs: {results.mean(): .2%} \\pm {results.std(): .2%}")
