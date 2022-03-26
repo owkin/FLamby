b7b83c6c46b240967a23032cc9b58be9b47edd5e
jeandut <jean.du-terrail@owkin.com>
2022-03-27 00:00:00 +0100

    caching tiled coords of the slide

--- flamby/datasets/fed_camelyon16/dataset_creation_scripts/tiling_slides.py
+++ flamby/datasets/fed_camelyon16/dataset_creation_scripts/tiling_slides.py
@@ -6,15 +6,17 @@ from glob import glob
 from pathlib import Path
 import itertools
 import numpy as np
+import pandas as pd
 import torch
 import torchvision.models as models
 from histolab.slide import Slide
 from histolab.tiler import GridTiler
 from skimage import io
 from torch.nn import Identity
-from torch.utils.data import DataLoader, IterableDataset
+from torch.utils.data import DataLoader, IterableDataset, Dataset
 from torchvision.transforms import Compose, Normalize, ToTensor
 from tqdm import tqdm
+from openslide import open_slide
 
 from flamby.utils import read_config, write_value_in_config
 
@@ -25,22 +27,54 @@ class SlideDataset(IterableDataset):
         self.it = grid_tiles_extractor._tiles_generator(slide)
     def __iter__(self):
         for tile in self.it:
-            im = self.transform(tile[0].image.convert("RGB"))
+            im = tile[0].image.convert("RGB")
+            if self.transform is not None:
+                im = self.transform(im)
             coords = tile[1]._asdict()
-            coords_list = []
-            for k, v in coords.items():
-                coords_list.append(v)
-            coords = torch.Tensor(coords_list)
+            coords = torch.Tensor([coords["x_ul"], coords["y_ul"]]).long()
             yield im, coords
 
+class DatasetFromCoords(Dataset):
+    def __init__(self, coords, slide_path, tile_size=224, level=1, transform=None):
+        self.transform = transform
+        self.coords = coords
+        self.slide = open_slide(slide_path)
+        self.level = level
+        self.tile_size = tile_size
+        self.transform = transform
 
-def main(batch_size, remove_big_tiff):
+    def __len__(self):
+        return self.coords.shape[0]
+        
+    def __getitem__(self, idx):
+        pil_image = self.slide.read_region(self.coords[idx], self.level, (self.tile_size, self.tile_size)).convert("RGB")
+        if self.transform is not None:
+            pil_image = self.transform(pil_image)
+        return pil_image, self.coords[idx]
+
+def dict_to_df(dict_arg):
+    return pd.DataFrame().from_dict(dict_arg)
+
+def save_dict_to_csv(dict_arg, file_name):
+    df = dict_to_df(dict_arg)
+    df.to_csv(file_name, index=False)
+
+
+
+def main(batch_size, num_workers_torch, tile_from_scratch, remove_big_tiff):
     """Function tiling slides that have been downloaded using download.py.
 
     Parameters
     ----------
     batch_size : int
         The number of images to use for batched inference in pytorch.
+
+    num_workers_torch: int
+        The number of parallel torch worker if precomputed coords allowed.
+
+    tile_from_scratch: bool
+        If this option is activated we disregard the csv files with precomputed
+        coordinates.
 
     remove_big_tiff : bool
         Whether or not to get rid of all original slides after tiling.
@@ -82,6 +116,7 @@ def main(batch_size, remove_big_tiff):
         print("You have already run the preprocessing, aborting.")
         sys.exit()
 
+    slides_dir = dict["dataset_path"]
     slides_paths = glob(os.path.join(slides_dir, "*.tif"))
     grid_tiles_extractor = GridTiler(
         tile_size=(224, 224),
@@ -111,33 +146,68 @@ def main(batch_size, remove_big_tiff):
     transform = Compose(
         [ToTensor(), Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])]
     )
+    path_to_coords_file = "tiling_coords.csv"
+    if not(os.path.exists(path_to_coords_file)):
+        df_predecessor = {"slide_name": [], "coords_x": [], "coords_y": []}
+    else:
+        df_predecessor = pd.read_csv(path_to_coords_file).to_dict()
     # Tiling all downloaded slides with provided model
     for sp in tqdm(slides_paths):
         slide_name = os.path.basename(sp)
-
         path_to_features = os.path.join(slides_dir, slide_name + ".npy")
         if os.path.exists(path_to_features):
             continue
         print(f"Tiling slide {sp}")
-        slide = Slide(sp, "./tmp", use_largeimage=True)
-        # We use the matter detection from histolab to tile the dataset
-        dataset_from_slide = SlideDataset(grid_tiles_extractor, slide, transform=transform)
-        dataloader = DataLoader(
-            dataset_from_slide,
-            batch_size=batch_size,
-            pin_memory=torch.cuda.is_available(),
-            num_workers=0,
-            drop_last=False,
-        )
+        # We use the matter detector from histolab to tile the dataset or the cached coords
+        df = dict_to_df(df_predecessor)
+        coords_from_slide = df.loc[df["slide_name"] == slide_name]
+        has_coords = len(coords_from_slide.index) > 0
+        print(f"HAS COORDS {has_coords}")
+        if has_coords and not(tile_from_scratch):
+            coords = coords_from_slide[["coords_x", "coords_y"]].to_numpy()
+            dataset_from_coords = DatasetFromCoords(coords, sp, transform=transform)
+            dataloader = DataLoader(
+                    dataset_from_coords,
+                    batch_size=batch_size,
+                    pin_memory=torch.cuda.is_available(),
+                    num_workers=num_workers_torch,
+                    drop_last=False,
+                )
+        else:
+            slide = Slide(sp, "./tmp", use_largeimage=True)
+            dataset_from_slide = SlideDataset(grid_tiles_extractor, slide, transform=transform)
+            # We cannot use multiprocessing per slide
+            dataloader = DataLoader(
+                dataset_from_slide,
+                batch_size=batch_size,
+                pin_memory=torch.cuda.is_available(),
+                num_workers=0,
+                drop_last=False,
+            )
         print(f"Extracting tiles and forwarding them with ResNet50 by batch of {batch_size}")
+        import time
+        t1 = time.time()
         with torch.inference_mode():
             features = np.zeros((0, 2048)).astype("float32")
+            coords = np.zeros((0, 2)).astype("int64")
             for i, (batch_images, batch_coords) in enumerate(iter(dataloader)):
                 if torch.cuda.is_available():
                     batch_images = batch_images.cuda()
                 features = np.concatenate((features, net(batch_images).detach().cpu().numpy()), axis=0)
-
+                coords = np.concatenate((coords, batch_coords.numpy()))
+        t2 = time.time()
+        print(f"Slide {slide_name} extraction lasted {t2-t1}s with has_coords={has_coords} (num_workers {num_workers_torch})")
+        if not(has_coords):
+            # We store the coordinates information to speed up the extraction process via multiprocessing
+            num_tiles_on_slide = coords.shape[0]
+            df_predecessor["slide_name"] += ([slide_name] * num_tiles_on_slide)
+            df_predecessor["coords_x"] += coords[:, 0].tolist()
+            df_predecessor["coords_y"] += coords[:, 1].tolist()
+            save_dict_to_csv(df_predecessor, path_to_coords_file)
+        # Saving features on dict
         np.save(path_to_features, features)
+
+    save_dict_to_csv(df_predecessor, path_to_coords_file)
 
     write_value_in_config(config_file, "preprocessing_complete", True)
 
@@ -156,6 +226,17 @@ if __name__ == "__main__":
         default=64,
     )
     parser.add_argument(
+        "--num-workers-torch",
+        type=int,
+        help="The number of torch workers for dataset with precomputed coords.",
+        default=10,
+    )
+    parser.add_argument(
+        "--tile-from-scratch",
+        action="store_true",
+        help="With this option we re-extract the matter from the slides.",
+    )
+    parser.add_argument(
         "--remove-big-tiff",
         action="store_true",
         help="Whether or not to remove the original slides images that take \
@@ -163,4 +244,4 @@ if __name__ == "__main__":
     )
 
     args = parser.parse_args()
-    main(args.batch_size, args.remove_big_tiff)
+    main(args.batch_size, args.num_workers_torch, args.tile_from_scratch, args.remove_big_tiff)
