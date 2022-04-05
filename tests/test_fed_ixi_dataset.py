c3b89d4e25b4222f25803a983b58366b9e74a395
Santiago SILVA <16252054+sssilvar@users.noreply.github.com>
2022-04-05 10:53:20 +0200

    Unit testing Generic interface IXI

--- tests/test_fed_ixi_dataset.py
+++ tests/test_fed_ixi_dataset.py
@@ -1,5 +1,19 @@
-def test_general_interface_instantiation():
+def test_generic_interface_instantiation():
+    # %%
     from flamby.datasets.fed_ixi import IXIDataset
 
     # Instantiate generic interface (no errors should be raised)
-    dataset = IXIDataset(root='~/.data/')
+    dataset = IXIDataset(root='~/Downloads/')
+
+    # Validate subject id verification
+    dataset._validate_subject_id('002')
+    dataset._validate_subject_id(2)
+
+    # Test len dataset
+    print('Dataset length:', len(dataset))
+
+    # Test subject search in TAR file
+    img = dataset[0]
+    print(img)
+
+
