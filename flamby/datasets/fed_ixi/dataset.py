2940970dce8af01e947a8b02de992f0dd2ade0c2
Santiago SILVA <16252054+sssilvar@users.noreply.github.com>
2022-04-05 07:40:16 +0200

    Generic interface for IXIDataset defined.

--- flamby/datasets/fed_ixi/dataset.py
+++ flamby/datasets/fed_ixi/dataset.py
@@ -1,5 +1,76 @@
+from os import PathLike
+from pathlib import Path
+from typing import Union, Iterable
+
 from torch.utils.data import Dataset
+
+import nibabel as nib
+import pandas as pd
 
 
 class IXIDataset(Dataset):
+    """
+    Generic interface for IXI Dataset
+
+    Parameters
+        ----------
+        root: str
+            Folder where data is or will be downloaded.
+        transform:
+            PyTorch Transform to process the data or augment it.
+    """
+
+    demographics = ['https://biomedic.doc.ic.ac.uk/brain-development/downloads/IXI/IXI.xls']
+    image_urls = []
+
+    def __init__(
+            self,
+            root: str,
+            transform=None
+    ):
+        self.root = Path(root).expanduser()
+        self.transform = transform
+
+    def download(self) -> None:
+        pass
+
+    def __getitem__(self, item) -> Iterable:
+        pass
+
+    def __len__(self) -> int:
+        pass
+
+
+class T1ImagesIXIDataset(IXIDataset):
     pass
+
+
+class T2ImagesIXIDataset(IXIDataset):
+    pass
+
+
+class PDImagesIXIDataset(IXIDataset):
+    pass
+
+
+class MRAImagesIXIDataset(IXIDataset):
+    pass
+
+
+class DTIImagesIXIDataset(IXIDataset):
+    pass
+
+
+class MultiModalIXIDataset(IXIDataset):
+    pass
+
+
+__all__ = [
+    'IXIDataset',
+    'T1ImagesIXIDataset',
+    'T2ImagesIXIDataset',
+    'PDImagesIXIDataset',
+    'MRAImagesIXIDataset',
+    'DTIImagesIXIDataset',
+    'MultiModalIXIDataset',
+]
