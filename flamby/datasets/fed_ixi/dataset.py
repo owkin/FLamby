446dfc0ce078195544dd3118092c204bc42e6633
Santiago SILVA <16252054+sssilvar@users.noreply.github.com>
2022-04-06 14:47:06 +0200

    Refactoring for better modularity.

--- flamby/datasets/fed_ixi/dataset.py
+++ flamby/datasets/fed_ixi/dataset.py
@@ -1,133 +1,13 @@
-import os
-from os import PathLike
 from pathlib import Path
 from tarfile import TarFile
-from typing import Union, Iterable, List, Tuple, Dict
+from typing import Union, Tuple, Dict
 
-import numpy
+import pandas as pd
+from monai.transforms import Resize, Compose, ToTensor, AddChannel
 from torch import Tensor
 from torch.utils.data import Dataset
 
-from monai.transforms import Resize, Compose, ToTensor, AddChannel
-
-import re
-import nibabel as nib
-import pandas as pd
-import tempfile
-
-
-def _get_id_from_filename(x, verify_single_matches=True) -> Union[List[int], int]:
-    """
-    Extract ID from NIFTI filename for cross-reference
-
-    Parameters
-    ----------
-    x : str
-        Basename of NIFTI file (e.g. `IXI652-Guys-1116-MRA.nii.gz`)
-
-    verify_single_matches: bool
-        Asserts the existance of a single match.
-        Used to verify that there exist only one image per subject and modality.
-
-    Returns
-    -------
-    list
-        A list containing the matched IDs found (each id has an integer type).
-    """
-    matches = re.findall(r'IXI\d{3}', x)
-    matches = [int(match[-3:]) for match in matches]
-
-    if verify_single_matches:
-        assert len(matches) == 1, 'One or more subjects are repeated into the ' \
-                                  'dataset. This is not expected behaviour.' \
-                                  'Please verify that there is only one image ' \
-                                  'per subject and modality.'
-        return matches[0]
-    return matches
-
-
-def _assembly_nifti_filename_regex(patient_id: int, modality: str) -> Union[str, PathLike, Path]:
-    """
-    Assembles NIFTI filename regular expression using the standard in the IXI dataset based on id and modality.
-
-    Parameters
-    ----------
-    patient_id: int
-        Patient's identifier.
-
-    modality: str
-        Image modality (e.g. `'T1'`).
-
-    Returns
-    -------
-
-    """
-    nii_filename = f'IXI{patient_id:03d}-[A-Za-z]+-[0-9]+-{modality.upper()}.nii.gz'
-    return nii_filename
-
-
-def _find_file_in_tar(tar_file: TarFile, patient_id: int, modality) -> str:
-    """
-    Searches the file in a TAR file that corresponds to a particular regular expression.
-
-    Parameters
-    ----------
-    patient_id: int
-        Patient's ID
-
-    Returns
-    -------
-    str
-        Filename corresponding to the particular subject's ID
-
-    Raises
-    -------
-    FileNotFoundError
-        If file was not found in TAR.
-    """
-    regex = _assembly_nifti_filename_regex(patient_id, modality)
-    filenames = tar_file.getnames()
-
-    for filename in filenames:
-        try:
-            return re.match(regex, filename).group()
-        except AttributeError:
-            continue
-
-    raise FileNotFoundError(f'File following the pattern {regex} could not be found.')
-
-
-def _extract_center_name_from_filename(filename: str):
-    """
-    Extracts center name from file dataset.
-
-    Unfortunately, IXI has the center encoded in the namefile rather than in the demographics information.
-
-    Parameters
-    ----------
-    filename: str
-        Basename of NIFTI file (e.g. `IXI652-Guys-1116-MRA.nii.gz`)
-
-    Returns
-    -------
-    str
-        Name of the center where the data comes from (e.g. Guys for the previous example)
-
-    """
-    # We decided to wrap a function for this for clarity and easier modularity for future expansion
-    return filename.split('-')[1]
-
-
-def _load_nifti_image_by_id(tar_file: TarFile, patient_id: int, modality) -> Tuple[
-    nib.Nifti1Header, numpy.ndarray, str]:
-    filename = _find_file_in_tar(tar_file, patient_id, modality)
-    with tempfile.TemporaryDirectory() as td:
-        full_path = os.path.join(td, filename)
-        tar_file.extract(filename, td)
-        nii_img = nib.load(full_path)
-        img = nii_img.get_fdata()
-        header = nii_img.get_header()
-    return header, img, _extract_center_name_from_filename(filename)
+from .utils import _get_id_from_filename, _load_nifti_image_by_id
 
 
 class IXIDataset(Dataset):
