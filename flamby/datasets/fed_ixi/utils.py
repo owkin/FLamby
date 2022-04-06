52a0b122cdbf15ed1ec0b4928e89de223505551b
Santiago SILVA <16252054+sssilvar@users.noreply.github.com>
2022-04-06 15:08:03 +0200

    PEP 257 and docs.

--- flamby/datasets/fed_ixi/utils.py
+++ flamby/datasets/fed_ixi/utils.py
@@ -1,28 +1,34 @@
+"""Federated IXI Dataset utils
+
+A set of function that allow data management suited for the `IXI dataset <https://brain-development.org/ixi-dataset/>`_.
+
+"""
+
 import os
 from os import PathLike
 import numpy
 from tarfile import TarFile
 from pathlib import Path
 
-
 import re
 import tempfile
 from typing import Union, List, Tuple
 
 import nibabel as nib
+from nibabel import Nifti1Header
+from numpy import ndarray
 
 
 def _get_id_from_filename(x, verify_single_matches=True) -> Union[List[int], int]:
-    """
-    Extract ID from NIFTI filename for cross-reference
+    """Extract ID from NIFTI filename for cross-reference
 
     Parameters
     ----------
     x : str
         Basename of NIFTI file (e.g. `IXI652-Guys-1116-MRA.nii.gz`)
 
-    verify_single_matches: bool
-        Asserts the existance of a single match.
+    verify_single_matches : bool
+        Asserts the existence of a single match.
         Used to verify that there exist only one image per subject and modality.
 
     Returns
@@ -43,12 +49,11 @@ def _get_id_from_filename(x, verify_single_matches=True) -> Union[List[int], int
 
 
 def _assembly_nifti_filename_regex(patient_id: int, modality: str) -> Union[str, PathLike, Path]:
-    """
-    Assembles NIFTI filename regular expression using the standard in the IXI dataset based on id and modality.
+    """Assembles NIFTI filename regular expression using the standard in the IXI dataset based on id and modality.
 
     Parameters
     ----------
-    patient_id: int
+    patient_id : int
         Patient's identifier.
 
     modality: str
@@ -63,12 +68,11 @@ def _assembly_nifti_filename_regex(patient_id: int, modality: str) -> Union[str,
 
 
 def _find_file_in_tar(tar_file: TarFile, patient_id: int, modality) -> str:
-    """
-    Searches the file in a TAR file that corresponds to a particular regular expression.
+    """Searches the file in a TAR file that corresponds to a particular regular expression.
 
     Parameters
     ----------
-    patient_id: int
+    patient_id : int
         Patient's ID
 
     Returns
@@ -94,14 +98,13 @@ def _find_file_in_tar(tar_file: TarFile, patient_id: int, modality) -> str:
 
 
 def _extract_center_name_from_filename(filename: str):
-    """
-    Extracts center name from file dataset.
+    """Extracts center name from file dataset.
 
     Unfortunately, IXI has the center encoded in the namefile rather than in the demographics information.
 
     Parameters
     ----------
-    filename: str
+    filename : str
         Basename of NIFTI file (e.g. `IXI652-Guys-1116-MRA.nii.gz`)
 
     Returns
@@ -114,8 +117,30 @@ def _extract_center_name_from_filename(filename: str):
     return filename.split('-')[1]
 
 
-def _load_nifti_image_by_id(tar_file: TarFile, patient_id: int, modality) -> Tuple[
-    nib.Nifti1Header, numpy.ndarray, str]:
+def _load_nifti_image_by_id(
+        tar_file: TarFile,
+        patient_id: int, modality) -> Tuple[nib.Nifti1Header, numpy.ndarray, str]:
+    """Loads NIFTI file from TAR file using a specific ID.
+
+    Parameters
+    ----------
+    tar_file : TarFile
+        `TarFile <https://docs.python.org/3/library/tarfile.html#tarfile-objects>`_ object
+    patient_id : int
+        Patient's ID whose image is to be extracted.
+    modality : str
+        MRI modality (e.g. `'T1'`).
+
+
+    Returns
+    -------
+    header : Nifti1Header
+        NIFTI headers proceeding from the image.
+    img : ndarray
+        NumPy array containing the intensities of the voxels.
+    center_name : str
+        Name of the center the file comes from. In IXI this is encoded only in the filename.
+    """
     filename = _find_file_in_tar(tar_file, patient_id, modality)
     with tempfile.TemporaryDirectory() as td:
         full_path = os.path.join(td, filename)
