ccd2cbc38ed6bd70eecfb8b5b22d9cf06e3f9173
Santiago SILVA <16252054+sssilvar@users.noreply.github.com>
2022-04-08 14:11:59 +0200

    Mark download test for specific execution.

--- tests/test_fed_ixi_dataset.py
+++ tests/test_fed_ixi_dataset.py
@@ -72,5 +72,6 @@ def test_dti_images_ixi_dataset():
     dataset_test_routine(dataset)
 
 
+@pytest.mark.skip(reason="Dataset is too large. Meat to be runed when necessary.")
 def test_dataset_download():
     assert False
