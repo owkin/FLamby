d696956879784f185700cfbe6623e17e220b6954
Regis LOEB <regis.loeb@owkin.com>
2022-04-21 10:00:57 +0200

    docstrings on dataset.py and function for training and evaluating Cox PH model

--- flamby/datasets/fed_tcga_brca/benchmark_lifelines.py
+++ flamby/datasets/fed_tcga_brca/benchmark_lifelines.py
@@ -1,15 +1,23 @@
 import lifelines
 from dataset import FedTcgaBrca
 
-penalizer = 0.1
-model = lifelines.CoxPHFitter(penalizer=penalizer)
 
-train = FedTcgaBrca(train=True, pooled=True).data
-train = train.drop("pid", 1)
-model.fit(train, "T", "E")
+def train_evaluate_Cox_prop_hazard_model_pooled():
 
-test = FedTcgaBrca(train=False, pooled=True).data
-test = test.drop("pid", 1)
-pred = -model.predict_partial_hazard(test)
-c_index = lifelines.utils.concordance_index(test["T"], pred, test["E"])
-print("Test c-index on pooled dataset ", c_index)
+    penalizer = 0.1
+    model = lifelines.CoxPHFitter(penalizer=penalizer)
+
+    train = FedTcgaBrca(train=True, pooled=True).data
+    train = train.drop("pid", 1)
+    model.fit(train, "T", "E")
+
+    test = FedTcgaBrca(train=False, pooled=True).data
+    test = test.drop("pid", 1)
+    pred = -model.predict_partial_hazard(test)
+    c_index = lifelines.utils.concordance_index(test["T"], pred, test["E"])
+    print("Test c-index on pooled dataset ", c_index)
+
+
+if __name__ == "__main__":
+
+    train_evaluate_Cox_prop_hazard_model_pooled()
