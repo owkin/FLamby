ba0cf966146c19c8ff5a7b4bfe0c8139f564de1f
jeandut <jean.du-terrail@owkin.com>
2022-04-26 19:38:58 +0200

    adding missing import in isic

--- flamby/datasets/fed_isic2019/__init__.py
+++ flamby/datasets/fed_isic2019/__init__.py
@@ -1,5 +1,4 @@
-from .common import BATCH_SIZE, LR, NUM_CLIENTS, NUM_EPOCHS_POOLED
-from .dataset import FedIsic2019, Isic2019Raw
+from .common import BATCH_SIZE, LR, NUM_CLIENTS, NUM_EPOCHS_POOLED, get_nb_max_rounds
 from .metric import metric
 from .model import Baseline
 from .loss import BaselineLoss
