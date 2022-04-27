5fc48bef1d5056f11e150b884358b5cd34a64c7d
jeandut <jean.du-terrail@owkin.com>
2022-04-27 10:53:32 +0200

    Update flamby/datasets/fed_isic2019/__init__.py

--- flamby/datasets/fed_isic2019/__init__.py
+++ flamby/datasets/fed_isic2019/__init__.py
@@ -1,4 +1,5 @@
 from .common import BATCH_SIZE, LR, NUM_CLIENTS, NUM_EPOCHS_POOLED, get_nb_max_rounds
+from .dataset import FedIsic2019, Isic2019Raw
 from .metric import metric
 from .model import Baseline
 from .loss import BaselineLoss
