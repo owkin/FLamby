b591609e1ae0a65c2d9b984b8345e768d8c5984e
jeandut <jean.du-terrail@owkin.com>
2022-05-06 14:11:20 +0200

    fixing circular imports

--- flamby/datasets/fed_tcga_brca/__init__.py
+++ flamby/datasets/fed_tcga_brca/__init__.py
@@ -1,5 +1,6 @@
 from .common import BATCH_SIZE, LR, NUM_CLIENTS, NUM_EPOCHS_POOLED
 from .dataset import FedTcgaBrca, TcgaBrcaRaw
+from .model import Baseline
 from .loss import BaselineLoss
 from .metric import metric
-from .model import Baseline
+
