8f0b0b33440150fa31d8b25c5eed6cc8675a78b3
jeandut <jean.du-terrail@owkin.com>
2022-05-09 11:11:12 +0200

    adding proper test for adam

--- tests/strategies/test_fed_adam.py
+++ tests/strategies/test_fed_adam.py
@@ -1,133 +1,110 @@
-import random
+import copy
 
 import numpy as np
 import pytest
 import torch
 from torch.utils.data import DataLoader as dl
+from torch.utils.data._utils.collate import default_collate
 
-from flamby.datasets.fed_tcga_brca import (
-    BATCH_SIZE,
-    Baseline,
-    BaselineLoss,
-    FedTcgaBrca,
-    metric,
-)
+from flamby.datasets.fed_tcga_brca import Baseline, BaselineLoss, FedTcgaBrca
 from flamby.strategies.fed_opt import FedAdam
-from flamby.utils import evaluate_model_on_tests
 
 
-def dict_mean(dict_list):
-    mean_dict = {}
-    for key in dict_list[0].keys():
-        mean_dict[key] = sum(d[key] for d in dict_list) / len(dict_list)
-    return mean_dict
+@pytest.mark.parametrize(
+    "seed, betas, epsilon",
+    [(42, (0.5, 0.8), 1.0), (43, (0.9, 0.999), 1e-8), (44, (0.5, 0.5), 1.0e-4)],
+)
+def test_fed_adam(seed, betas, epsilon):
+    """FedAdam is not entirely the same as Adam in the 1 client only case.
+    Notably there is no bias correction in FedAdam.
+    However by tweaking the server learning rate of FedAdam and under
+    certain conditions one can have the equality between both.
+    We need to use doubles as floats use cause frequent rounding errors,
+    making tests fail.
+    - client learning rate of 1. (LR)
+    - sever learning rate of {\sqrt{1. - \beta_{2}}}{1. - \beta_{1}}
+    - tau = epsilon \cdot \sqrt{1. - \beta_{2}}
+    - first iteration of both algorithms and num_updates = 1
 
+    Parameters
+    ----------
+    seed : int
+        The seed to test.
+    betas: tuple
+        The betas associated with ADAM to control both momentums.
+    epsilon: float
+        The numerical precision parameter of ADAM in order to avoid dividing by 0.
+    """
 
-def test_fed_adam():
-    lossfunc = BaselineLoss()
-    results0 = []
-    results1 = []
-    for seed in range(3):
-        torch.manual_seed(seed)
-        np.random.seed(seed)
+    LR = 1.0
+    slr = np.sqrt((1.0 - betas[1])) / (1.0 - betas[0])
+    num_updates = 1
+    num_rounds = 1
+    loss = BaselineLoss()
+    torch.manual_seed(seed)
 
-        train_dataset = FedTcgaBrca(train=True, pooled=True)
-        train_dataloader = torch.utils.data.DataLoader(
-            train_dataset, batch_size=BATCH_SIZE, shuffle=True, num_workers=4
-        )
-        test_dataset = FedTcgaBrca(train=False, pooled=True)
-        test_dataloader = torch.utils.data.DataLoader(
-            test_dataset,
-            batch_size=BATCH_SIZE,
+    m1 = Baseline().to(torch.double)
+    m2 = copy.deepcopy(m1)
+
+    def collate_fn_double(batch):
+        outputs = default_collate(batch)
+        return [o.to(torch.double) for o in outputs]
+
+    # FedAdam
+    torch.manual_seed(seed)
+    training_dataloaders = [
+        dl(
+            FedTcgaBrca(center=0, train=True, pooled=True),
+            batch_size=32,
             shuffle=False,
-            num_workers=4,
+            collate_fn=collate_fn_double,
         )
-        dataloaders = {"train": train_dataloader, "test": test_dataloader}
+    ]
 
-        device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-        model = Baseline()
-        model = model.to(device)
-        optimizer = torch.optim.Adam(model.fc.parameters(), lr=0.1)
-        scheduler = torch.optim.lr_scheduler.ConstantLR(optimizer, factor=1.0)
+    s = FedAdam(
+        training_dataloaders,
+        m1,
+        loss,
+        torch.optim.SGD,
+        LR,
+        num_updates=num_updates,
+        nrounds=num_rounds,
+        log=False,
+        tau=epsilon * np.sqrt((1.0 - betas[1])),
+        server_learning_rate=slr,
+        beta1=betas[0],
+        beta2=betas[1],
+    )
+    m1 = s.run()[0]
+    weights_model_from_strat = [p.detach().numpy() for p in m1.parameters()]
 
-        results0.append(evaluate_model_on_tests(model, [test_dataloader], metric))
-
-        for idx, (X, y) in enumerate(dataloaders["train"]):
-            if idx == 0:
-                X = X.to(device)
-                y = y.to(device)
-                optimizer.zero_grad()
-                outputs = model(X)
-                loss = lossfunc(outputs, y)
-                loss.backward()
-                optimizer.step()
-                scheduler.step()
-
-        dict_cindex = evaluate_model_on_tests(model, [dataloaders["test"]], metric)
-        results1.append(dict_cindex)
-
-    print("Before training")
-    print("Test C-index ", results0)
-    print("Average test C-index ", dict_mean(results0))
-    print("After training")
-    print("Test C-index ", results1)
-    print("Average test C-index ", dict_mean(results1))
-
-    results2 = []
-    results3 = []
-    for seed in range(3):
-        torch.manual_seed(seed)
-        np.random.seed(seed)
-        NUM_CLIENTS = 1
-
-        training_dls = [
-            dl(
-                FedTcgaBrca(train=True, pooled=True),
-                batch_size=BATCH_SIZE,
-                shuffle=True,
-                num_workers=4,
-            )
-            for i in range(NUM_CLIENTS)
-        ]
-        test_dls = [
-            dl(
-                FedTcgaBrca(train=False, pooled=True),
-                batch_size=BATCH_SIZE,
-                shuffle=False,
-                num_workers=4,
-            )
-            for i in range(NUM_CLIENTS)
-        ]
-        loss = BaselineLoss()
-        m = Baseline()
-        results2.append(evaluate_model_on_tests(m, test_dls, metric))
-
-        NUM_UPDATES = 1
-        nrounds = 1
-
-        optimizer_class = torch.optim.SGD
-        LR = 1e0
-        SLR = (1e-1) * np.sqrt((1.0 - 0.999)) / (1.0 - 0.9)
-        TAU = (1e-8) * ((1 - 0.999) ** 0.5)
-
-        s = FedAdam(
-            training_dls,
-            m,
-            loss,
-            optimizer_class,
-            LR,
-            NUM_UPDATES,
-            nrounds,
-            log=False,
-            tau=TAU,
-            server_learning_rate=SLR,
+    # Reference
+    torch.manual_seed(seed)
+    training_dataloaders = [
+        dl(
+            FedTcgaBrca(center=0, train=True, pooled=True),
+            batch_size=32,
+            shuffle=False,
+            collate_fn=collate_fn_double,
         )
-        m = s.run()
-        results3.append(evaluate_model_on_tests(m[0], test_dls, metric))
+    ]
+    it = iter(training_dataloaders[0])
 
-    print("Test metric before training")
-    print(results2)
-    print(dict_mean(results2))
-    print("Test metric after training")
-    print(results3)
-    print(dict_mean(results3))
+    adam_optim = torch.optim.Adam(m2.parameters(), lr=LR, betas=betas, eps=epsilon)
+
+    for i in range(num_updates):
+        adam_optim.zero_grad()
+        X, y = next(it)
+        y_pred = m2(X)
+        ls = loss(y_pred, y)
+        ls.backward()
+        adam_optim.step()
+
+    weights_model_from_adam = [p.detach().numpy() for p in m2.parameters()]
+
+    assert all(
+        [
+            np.allclose(w1, w2)
+            for w1, w2 in zip(weights_model_from_strat, weights_model_from_adam)
+        ]
+    )
