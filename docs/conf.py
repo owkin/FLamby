58ea17db975cb7cea6ed6ca08e87c7c861eb123d
jeandut <jean.du-terrail@owkin.com>
2022-05-11 23:11:13 +0200

    using the proper sphinx version and adding dependencies

--- docs/conf.py
+++ docs/conf.py
@@ -32,7 +32,7 @@ release = "0.0.1"
 
 sys.path.insert(0, os.path.abspath(".."))
 
-
+autodoc_mock_imports = ["numpy", "albumentations"]
 # -- General configuration ---------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
