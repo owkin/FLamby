76b96507b8a8e352a1c69ceb5659a53b7b56d444
jeandut <jean.du-terrail@owkin.com>
2022-05-11 18:32:05 +0200

    first try at the sphinx docs, some stuff are still not building

--- flamby/strategies/__init__.py
+++ flamby/strategies/__init__.py
@@ -1,4 +1,5 @@
+from .cyclic import Cyclic
 from .fed_avg import FedAvg
 from .fed_opt import FedAdagrad, FedAdam, FedYogi
-from .utils import _Model, check_exchange_compliance
 from .fed_prox import FedProx
+from .utils import _Model, check_exchange_compliance
