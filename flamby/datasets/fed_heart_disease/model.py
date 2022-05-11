f4922c51f86733c91de599bb69fa92a0c4fb47f6
jeandut <jean.du-terrail@owkin.com>
2022-05-11 12:04:15 +0200

    Adding default values

--- flamby/datasets/fed_heart_disease/model.py
+++ flamby/datasets/fed_heart_disease/model.py
@@ -1,9 +1,8 @@
 import torch
 import torch.nn as nn
 
-
 class Baseline(nn.Module):
-    def __init__(self, input_dim, output_dim):
+    def __init__(self, input_dim=16, output_dim=1):
         super(Baseline, self).__init__()
         self.linear = torch.nn.Linear(input_dim, output_dim)
 
