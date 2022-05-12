c48b1962b0c7701fa7aa66ec50f6b6dd69bc6ead
saipraneet <saipraneet@gmail.com>
2022-05-11 18:41:03 -0700

    added scaffold to strategies init

--- flamby/strategies/__init__.py
+++ flamby/strategies/__init__.py
@@ -2,4 +2,5 @@ from .cyclic import Cyclic
 from .fed_avg import FedAvg
 from .fed_opt import FedAdagrad, FedAdam, FedYogi
 from .fed_prox import FedProx
+from .scaffold import Scaffold
 from .utils import _Model, check_exchange_compliance
