e2f96711b44e4b1022e7a8da8ff9584f0db1ca9f
regloeb <regis.loeb@owkin.com>
2022-05-13 19:01:16 +0200

    added a shuffling of the data before assiging train or test + new metric that can handle center 2 (it is single class)

--- flamby/datasets/fed_heart_disease/metric.py
+++ flamby/datasets/fed_heart_disease/metric.py
@@ -7,6 +7,9 @@ def metric(y_true, y_pred):
     # The try except is needed because when the metric is batched some batches
     # have one class only
     try:
-        return roc_auc_score(y_true, y_pred)
+        # return roc_auc_score(y_true, y_pred)
+        # proposed modification in order to get a metric that calcs on center 2
+        # (y=1 only on that center)
+        return ((y_pred > 0.5) == y_true).mean()
     except ValueError:
         return np.nan
