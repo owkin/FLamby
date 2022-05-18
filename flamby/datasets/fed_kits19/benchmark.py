9c9b41b2a350c6d23e255ce5d53c92292013a129
ErumMushtaq <emushtaq@usc.edu>
2022-05-17 22:03:43 -0700

    Benchmark function cleanup

--- flamby/datasets/fed_kits19/benchmark.py
+++ flamby/datasets/fed_kits19/benchmark.py
@@ -85,48 +85,36 @@ def train_model(
                         optimizer.step()
                         running_loss += loss.item() * inputs.size(0)
 
-                    # if test: record testing accuracy
+                    # if test: record dice
                     if phase == "test":
                         preds_softmax = softmax_helper(outputs)
                         preds = preds_softmax.argmax(1)
                         dice_score = metric(preds.cpu(), labels.cpu())
                         dice_list.append(dice_score)
 
-                    break
-                break
-
-
-            # break
             if phase == "train":
                 scheduler.step(epoch)
 
-            break
 
         epoch_loss = running_loss / dataset_sizes[phase]
-        epoch_acc = (np.mean(dice_list))
+        epoch_acc = (np.mean(dice_list)) # average dice
 
         if epoch_acc > best_acc:
             best_acc = epoch_acc
             best_model_wts = copy.deepcopy(model.state_dict())
 
-        print(
-                "{} Loss: {:.4f} Acc: {:.4f} ".format(
-                    phase, epoch_loss, epoch_acc
-                )
-            )
+        print("Loss: {:.4f} Acc: {:.4f} ".format(epoch_loss, epoch_acc))
         training_loss_list.append(epoch_loss)
         training_dice_list.append(epoch_acc)
-        break
 
 
     time_elapsed = time.time() - since
-    print(
-        "Training complete in {:.0f}m {:.0f}s".format(
-            time_elapsed // 60, time_elapsed % 60
-        )
-    )
+    print("Training complete in {:.0f}m {:.0f}s".format(time_elapsed // 60, time_elapsed % 60))
     print("Best test Balanced acc: {:4f}".format(best_acc))
-
+    print('----- Training Loss ---------')
+    print(training_loss_list)
+    print('------Validation Accuracy ------')
+    print(training_dice_list)
     # load best model weights
     model.load_state_dict(best_model_wts)
     return model
@@ -173,8 +161,9 @@ def main(args):
                                                        verbose=True, threshold=1e-3,
                                                        threshold_mode="abs")
 
-    #TODO: Add seeds
-    #TODO: Test train model on lambda 5
+    #TODO: Add 5 seeds
+    torch.manual_seed(0)
+    #TODO: Test train model on lambda 5 (Preprocessing running)
     model = train_model(
         model,
         optimizer,
@@ -185,7 +174,7 @@ def main(args):
         lossfunc,
         NUM_EPOCHS_POOLED,
     )
-
+    print('----- Test Accuracy ----------')
     print(evaluate_dice_on_tests(model, [test_dataloader], metric, use_gpu=True))
 
 
