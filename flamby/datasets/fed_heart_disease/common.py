b7019eebd2da46ae6eb3ce6fb779f33bf005cad6
regloeb <regis.loeb@owkin.com>
2022-05-18 11:57:53 +0200

    bug fixes experiment scipt, run script on tcga and heart

--- flamby/datasets/fed_heart_disease/common.py
+++ flamby/datasets/fed_heart_disease/common.py
@@ -1,8 +1,10 @@
-NUM_CLIENTS = 4
-BATCH_SIZE = 32
-NUM_EPOCHS_POOLED = 50
-LR = 0.05
+import torch
 
+NUM_CLIENTS = 4
+BATCH_SIZE = 8
+NUM_EPOCHS_POOLED = 50
+LR = 0.001
+Optimizer = torch.optim.Adam
 
 def get_nb_max_rounds(num_updates, batch_size=BATCH_SIZE):
-    return (495 // BATCH_SIZE) * NUM_EPOCHS_POOLED // num_updates
+    return (486 // BATCH_SIZE) * NUM_EPOCHS_POOLED // num_updates
