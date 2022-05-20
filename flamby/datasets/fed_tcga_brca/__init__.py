8d40683dcc54cfb1fb3e2e8e79e4311cbe02e200
maria.telenczuk <maria.telenczuk@owkin.com>
2022-05-20 15:19:59 +0200

    update skipping isort  and refacto

--- flamby/datasets/fed_tcga_brca/__init__.py
+++ flamby/datasets/fed_tcga_brca/__init__.py
@@ -7,6 +7,7 @@ from .common import (
     get_nb_max_rounds,
 )
 from .dataset import FedTcgaBrca, TcgaBrcaRaw
-from .loss import BaselineLoss
 from .metric import metric
-from .model import Baseline  # NOQA
+
+from .model import Baseline  # isort: skip
+from .loss import BaselineLoss  # isort: skip depends on Baseline
