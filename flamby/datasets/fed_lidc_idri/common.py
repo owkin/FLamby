3ae2f2cd2316985f91404a802d1b7eed7fb303ef
jeandut <jeandut@hotmail.com>
2022-05-24 13:50:42 +0000

    true number of record for lidc training

--- flamby/datasets/fed_lidc_idri/common.py
+++ flamby/datasets/fed_lidc_idri/common.py
@@ -10,4 +10,4 @@ Optimizer = torch.optim.Adam
 
 
 def get_nb_max_rounds(num_updates, batch_size=BATCH_SIZE):
-    return (800 // NUM_CLIENTS // batch_size) * NUM_EPOCHS_POOLED // num_updates
+    return (805 // NUM_CLIENTS // batch_size) * NUM_EPOCHS_POOLED // num_updates
