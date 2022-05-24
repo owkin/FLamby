6dafee912210c3b8d7f05fddff639cc309d7780b
jeandut <jeandut@hotmail.com>
2022-05-24 13:31:04 +0000

    adding arguments for kits19 as well

--- flamby/datasets/fed_kits19/common.py
+++ flamby/datasets/fed_kits19/common.py
@@ -1,5 +1,12 @@
+import torch
+
 NUM_CLIENTS = 6
 BATCH_SIZE = 2
 NUM_EPOCHS_POOLED = 2000
 LR = 3e-4
 SEEDS = [0, 10, 20, 30, 40]
+Optimizer = torch.optim.Adam
+
+
+def get_nb_max_rounds(num_updates):
+    return (72 // NUM_CLIENTS // BATCH_SIZE) * NUM_EPOCHS_POOLED // num_updates
