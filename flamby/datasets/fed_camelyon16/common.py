e155c118efcc0cfbeadc4ccad56c00e0f96bb3e6
jeandut <jeandut@hotmail.com>
2022-05-24 13:27:14 +0000

    fixing get_nb_max_rounds

--- flamby/datasets/fed_camelyon16/common.py
+++ flamby/datasets/fed_camelyon16/common.py
@@ -1,8 +1,12 @@
+import torch
+
 NUM_CLIENTS = 2
 BATCH_SIZE = 16
 NUM_EPOCHS_POOLED = 15
 LR = 0.001
 
+Optimizer = torch.optim.Adam
+
 
 def get_nb_max_rounds(num_updates, batch_size=BATCH_SIZE):
-    return (270 // BATCH_SIZE) * NUM_EPOCHS_POOLED // num_updates
+    return (270 // NUM_CLIENTS // BATCH_SIZE) * NUM_EPOCHS_POOLED // num_updates
