7e3bfc926ea528657d5396b65b4aa480d54cf8f6
jeandut <jeandut@hotmail.com>
2022-05-24 13:38:56 +0000

    exhaustive fixing of common.py files

--- flamby/datasets/fed_lidc_idri/common.py
+++ flamby/datasets/fed_lidc_idri/common.py
@@ -1,5 +1,13 @@
+import torch
+
 NUM_CLIENTS = 4
 NUM_EPOCHS_POOLED = 100
 BATCH_SIZE = 1
 LR = 1e-2
 SEEDS = [42]
+
+Optimizer = torch.optim.Adam
+
+
+def get_nb_max_rounds(num_updates, batch_size=BATCH_SIZE):
+    return (800 // NUM_CLIENTS // BATCH_SIZE) * NUM_EPOCHS_POOLED // num_updates
