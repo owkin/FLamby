d965637c10225d0ecd2e46d6597389b981ade48f
jeandut <jean.du-terrail@owkin.com>
2022-05-25 15:40:45 +0000

    adding paths to the right imports

--- flamby/datasets/fed_lidc_idri/__init__.py
+++ flamby/datasets/fed_lidc_idri/__init__.py
@@ -1,4 +1,12 @@
-from .common import BATCH_SIZE, LR, NUM_CLIENTS, NUM_EPOCHS_POOLED, SEEDS
+from .common import (
+    BATCH_SIZE,
+    LR,
+    NUM_CLIENTS,
+    NUM_EPOCHS_POOLED,
+    SEEDS,
+    Optimizer,
+    get_nb_max_rounds,
+)
 from .dataset import FedLidcIdri, LidcIdriRaw, collate_fn
 from .loss import BaselineLoss
 from .metric import evaluate_dice_on_tests_by_chunks, metric
