83990a943c24ad16f409f6be0609ade3b18aae81
jeandut <jean.du-terrail@owkin.com>
2022-05-26 11:48:47 +0200

    fixing doc and imports

--- flamby/datasets/fed_kits19/__init__.py
+++ flamby/datasets/fed_kits19/__init__.py
@@ -1,5 +1,5 @@
 from .common import BATCH_SIZE, LR, NUM_CLIENTS, NUM_EPOCHS_POOLED
-from .dataset import FedKits19, KiTS19Raw
+from .dataset import FedKits19, Kits19Raw
 from .metric import metric, evaluate_dice_on_tests, softmax_helper
 from .model import Baseline
 from .loss import BaselineLoss
