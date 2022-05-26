66d22939eb54a91f75cfff24e84ebf5a18238c87
ErumMushtaq <emushtaq@usc.edu>
2022-05-25 18:52:35 -0700

    Optimizer NoneType error fix

--- flamby/datasets/fed_kits19/__init__.py
+++ flamby/datasets/fed_kits19/__init__.py
@@ -1,4 +1,4 @@
-from .common import BATCH_SIZE, LR, NUM_CLIENTS, NUM_EPOCHS_POOLED
+from .common import BATCH_SIZE, LR, NUM_CLIENTS, NUM_EPOCHS_POOLED, Optimizer, get_nb_max_rounds
 from .dataset import FedKits19, KiTS19Raw
 from .metric import metric, evaluate_dice_on_tests, softmax_helper
 from .model import Baseline
