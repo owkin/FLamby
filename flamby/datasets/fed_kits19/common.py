10cc746471de2c9078b908e686724633c321649a
ErumMushtaq <emushtaq@usc.edu>
2022-05-26 17:22:59 -0700

    Update common.py

--- flamby/datasets/fed_kits19/common.py
+++ flamby/datasets/fed_kits19/common.py
@@ -2,9 +2,9 @@ import torch
 
 NUM_CLIENTS = 6
 BATCH_SIZE = 2
-NUM_EPOCHS_POOLED = 5
+NUM_EPOCHS_POOLED = 3000
 LR = 3e-4
-SEEDS = [0, 10, 20, 30, 40]
+SEEDS = [0]
 Optimizer = torch.optim.Adam
 
 
