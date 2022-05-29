fab3f16a8718bf02aa8393f12244440cae6c7e06
Samy Ayed <ayedsamy25@gmail.com>
2022-05-29 16:20:09 +0200

    init update

--- flamby/datasets/fed_ixi/__init__.py
+++ flamby/datasets/fed_ixi/__init__.py
@@ -1,4 +1,12 @@
-from .common import BATCH_SIZE, NUM_CLIENTS, NUM_EPOCHS_POOLED, SEEDS
+from .common import (
+    BATCH_SIZE,
+    LR,
+    NUM_CLIENTS,
+    NUM_EPOCHS_POOLED,
+    SEEDS,
+    Optimizer,
+    get_nb_max_rounds,
+)
 from .dataset import FedIXITiny, IXITinyRaw
 from .loss import BaselineLoss
 from .metric import metric
