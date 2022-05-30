fb6ad0041110f19ad464219abf7cae67881542e0
jeandut <jean.du-terrail@owkin.com>
2022-05-30 19:28:50 +0200

    splitting fed_benchmark for parallelization

--- flamby/fed_benchmark.py
+++ flamby/fed_benchmark.py
@@ -114,7 +114,7 @@ def main(args_cli):
     else:
         batch_size_test = None
         evaluate_func = evaluate_model_on_tests
-        compute_ensemble_perf = True
+        compute_ensemble_perf = False
 
     nb_local_and_ensemble_xps = (NUM_CLIENTS + int(compute_ensemble_perf)) * (
         NUM_CLIENTS + 1
@@ -151,7 +151,36 @@ def main(args_cli):
         df = pd.DataFrame(columns=columns_names)
         perf_lines_dicts = []
 
+    do_strategy = True
+    do_baseline = {"Pooled": True}
+    for i in range(NUM_CLIENTS):
+        do_baseline[f"Local {i}"] = True
     # Single client baseline computation
+    if args_cli.singe_centric_baseline is not None:
+        do_baseline = {"Pooled": False}
+        for i in range(NUM_CLIENTS):
+            do_baseline[f"Local {i}"] = False
+        assert (
+            args_cli.singe_centric_baseline in do_baseline
+        ), f"{args_cli.singe_centric_baseline} not in Pooled or Local 0,...,n-1"
+        assert args_cli.num_local in range(
+            NUM_CLIENTS
+        ), "The client you chose does not exist"
+        do_baseline[args_cli.singe_centric_baseline] = True
+        # If we do a single-centric baseline we don't do the strategies
+        do_strategy = False
+
+    # if we give a strategy we compute only the strategy and not the baselines
+    if args_cli.strategy is not None:
+        for k, _ in do_baseline.items():
+            do_baseline[k] = False
+
+    do_all_local = all([do_baseline[f"Local {i}"] for i in range(NUM_CLIENTS)])
+    if compute_ensemble_perf and not (do_all_local):
+        raise ValueError(
+            "Cannot compute ensemble performance if training on only one local"
+        )
+
     # We use the same set of parameters as found in the corresponding
     # flamby/datasets/fed_mydataset/benchmark.py
     # Pooled Baseline
@@ -159,7 +188,7 @@ def main(args_cli):
     # yet. Note that pooled and local baselines do not use hyperparameters.
     index_of_interest = df.loc[df["Method"] == "Pooled Training"].index
     # an experiment is finished if there are num_clients + 1 rows
-    if len(index_of_interest) < (NUM_CLIENTS + 1):
+    if (len(index_of_interest) < (NUM_CLIENTS + 1)) and do_baseline["Pooled"]:
         # dealing with edge case that shouldn't happen
         # If some of the rows are there but not all of them we redo the experiments
         if len(index_of_interest) > 0:
@@ -243,7 +272,9 @@ def main(args_cli):
             opt = Optimizer(m.parameters(), lr=LR)
             index_of_interest = df.loc[df["Method"] == f"Local {i}"].index
 
-            if (len(index_of_interest) < (NUM_CLIENTS + 1)) or compute_ensemble_perf:
+            if (
+                (len(index_of_interest) < (NUM_CLIENTS + 1)) or compute_ensemble_perf
+            ) and do_baseline[f"Local {i}"]:
                 if len(index_of_interest) > 0:
                     df.drop(index_of_interest, inplace=True)
                     perf_lines_dicts = df.to_dict("records")
@@ -360,142 +391,147 @@ def main(args_cli):
             df.to_csv(results_file, index=False)
 
     # Strategies
-    for num_updates in run_num_updates:
-        for sname in strategy_specific_hp_dicts.keys():
-            # Base arguments
-            m = copy.deepcopy(global_init)
-            bloss = BaselineLoss()
-            args = {
-                "training_dataloaders": training_dls,
-                "model": m,
-                "loss": bloss,
-                "optimizer_class": Optimizer,
-                "learning_rate": LR,
-                "num_updates": num_updates,
-                "nrounds": get_nb_max_rounds(num_updates),
-            }
-            strategy_specific_hp_dict = strategy_specific_hp_dicts[sname]
-            # Overwriting arguments with strategy specific arguments
-            for k, v in strategy_specific_hp_dict.items():
-                args[k] = v
-            # We only launch training if it's not finished already. Maybe FL
-            # hyperparameters need to be tuned.
-            hyperparameters = {}
-            for k in hp_additional_args:  # columns_names:
-                if k in args:
-                    hyperparameters[k] = args[k]
-                else:
-                    hyperparameters[k] = np.nan
-            # This is very ugly but this is the only way I found to accomodate float
-            # and objects equality in a robust fashion
-            found_xps = df[list(hyperparameters)]
-            found_xps_numerical = found_xps.select_dtypes(exclude=[object])
-            col_numericals = found_xps_numerical.columns
-            col_objects = [c for c in found_xps.columns if not (c in col_numericals)]
-
-            if len(col_numericals) > 0:
-                bool_numerical = np.all(
-                    np.isclose(
-                        found_xps_numerical,
-                        pd.Series(
-                            {
-                                k: hyperparameters[k]
-                                for k in list(hyperparameters.keys())
-                                if k in col_numericals
-                            }
-                        ),
-                        equal_nan=True,
-                    ),
-                    axis=1,
-                )
-            else:
-                bool_numerical = np.ones((len(df.index), 1)).astype("bool")
-            if len(col_objects):
-                bool_objects = found_xps[col_objects].astype(str) == pd.Series(
-                    {
-                        k: str(hyperparameters[k])
-                        for k in list(hyperparameters.keys())
-                        if k in col_objects
-                    }
-                )
-            else:
-                bool_objects = np.ones((len(df.index), 1)).astype("bool")
-            bool_method = df["Method"] == (sname + str(num_updates))
-            index_of_interest_1 = df.loc[pd.DataFrame(bool_numerical).all(axis=1)].index
-            index_of_interest_2 = df.loc[pd.DataFrame(bool_objects).all(axis=1)].index
-            index_of_interest_3 = df.loc[pd.DataFrame(bool_method).all(axis=1)].index
-            index_of_interest = index_of_interest_1.intersection(
-                index_of_interest_2
-            ).intersection(index_of_interest_3)
-            # non-robust version
-            # index_of_interest = df.loc[
-            #   (df["Method"] == (sname + str(num_updates)))
-            #   & (
-            #       df[list(hyperparameters)] == pd.Series(hyperparameters)
-            #   ).all(axis=1)
-            # ].index
-            # An experiment is finished if there are num_clients + 1 rows
-            if len(index_of_interest) < (NUM_CLIENTS + 1):
-                # Dealing with edge case that shouldn't happen
-                # If some of the rows are there but not all of them we redo the
-                # experiments
-                if len(index_of_interest) > 0:
-                    df.drop(index_of_interest, inplace=True)
-                    perf_lines_dicts = df.to_dict("records")
-                basename = dataset_name + "-" + sname + f"-num-updates{num_updates}"
-                for k, v in args.items():
-                    if k in ["learning_rate", "server_learning_rate"]:
-                        basename += (
-                            "-" + "".join([e[0] for e in str(k).split("_")]) + str(v)
-                        )
-                    if k in ["mu", "deterministic_cycle"]:
-                        basename += "-" + str(k) + str(v)
-
-                # We run the FL strategy
-                s = getattr(strats, sname)(
-                    **args, log=args_cli.log, log_basename=basename
-                )
-                print("FL strategy", sname, " num_updates ", num_updates)
-                m = s.run()[0]
-
-                perf_dict = evaluate_func(m, test_dls, metric)
-                pooled_perf_dict = evaluate_func(m, [test_pooled], metric)
-                print("Per-center performance:")
-                print(perf_dict)
-                print("Performance on pooled test set:")
-                print(pooled_perf_dict)
-                hyperparams_save = {
-                    k: v
-                    for k, v in hyperparameters.items()
-                    if k not in init_hp_additional_args
+    if do_strategy:
+        for num_updates in run_num_updates:
+            for sname in strategy_specific_hp_dicts.keys():
+                # Base arguments
+                m = copy.deepcopy(global_init)
+                bloss = BaselineLoss()
+                args = {
+                    "training_dataloaders": training_dls,
+                    "model": m,
+                    "loss": bloss,
+                    "optimizer_class": Optimizer,
+                    "learning_rate": LR,
+                    "num_updates": num_updates,
+                    "nrounds": get_nb_max_rounds(num_updates),
                 }
-                for k, v in perf_dict.items():
+                strategy_specific_hp_dict = strategy_specific_hp_dicts[sname]
+                # Overwriting arguments with strategy specific arguments
+                for k, v in strategy_specific_hp_dict.items():
+                    args[k] = v
+                # We only launch training if it's not finished already. Maybe FL
+                # hyperparameters need to be tuned.
+                hyperparameters = {}
+                for k in hp_additional_args:  # columns_names:
+                    if k in args:
+                        hyperparameters[k] = args[k]
+                    else:
+                        hyperparameters[k] = np.nan
+                # This is very ugly but this is the only way I found to accomodate float
+                # and objects equality in a robust fashion
+                found_xps = df[list(hyperparameters)]
+                found_xps_numerical = found_xps.select_dtypes(exclude=[object])
+                col_numericals = found_xps_numerical.columns
+                col_objects = [c for c in found_xps.columns if not (c in col_numericals)]
+
+                if len(col_numericals) > 0:
+                    bool_numerical = np.all(
+                        np.isclose(
+                            found_xps_numerical,
+                            pd.Series(
+                                {
+                                    k: hyperparameters[k]
+                                    for k in list(hyperparameters.keys())
+                                    if k in col_numericals
+                                }
+                            ),
+                            equal_nan=True,
+                        ),
+                        axis=1,
+                    )
+                else:
+                    bool_numerical = np.ones((len(df.index), 1)).astype("bool")
+                if len(col_objects):
+                    bool_objects = found_xps[col_objects].astype(str) == pd.Series(
+                        {
+                            k: str(hyperparameters[k])
+                            for k in list(hyperparameters.keys())
+                            if k in col_objects
+                        }
+                    )
+                else:
+                    bool_objects = np.ones((len(df.index), 1)).astype("bool")
+                bool_method = df["Method"] == (sname + str(num_updates))
+                index_of_interest_1 = df.loc[
+                    pd.DataFrame(bool_numerical).all(axis=1)
+                ].index
+                index_of_interest_2 = df.loc[
+                    pd.DataFrame(bool_objects).all(axis=1)
+                ].index
+                index_of_interest_3 = df.loc[pd.DataFrame(bool_method).all(axis=1)].index
+                index_of_interest = index_of_interest_1.intersection(
+                    index_of_interest_2
+                ).intersection(index_of_interest_3)
+                # non-robust version
+                # index_of_interest = df.loc[
+                #   (df["Method"] == (sname + str(num_updates)))
+                #   & (
+                #       df[list(hyperparameters)] == pd.Series(hyperparameters)
+                #   ).all(axis=1)
+                # ].index
+                # An experiment is finished if there are num_clients + 1 rows
+                if len(index_of_interest) < (NUM_CLIENTS + 1):
+                    # Dealing with edge case that shouldn't happen
+                    # If some of the rows are there but not all of them we redo the
+                    # experiments
+                    if len(index_of_interest) > 0:
+                        df.drop(index_of_interest, inplace=True)
+                        perf_lines_dicts = df.to_dict("records")
+                    basename = dataset_name + "-" + sname + f"-num-updates{num_updates}"
+                    for k, v in args.items():
+                        if k in ["learning_rate", "server_learning_rate"]:
+                            basename += (
+                                "-" + "".join([e[0] for e in str(k).split("_")]) + str(v)
+                            )
+                        if k in ["mu", "deterministic_cycle"]:
+                            basename += "-" + str(k) + str(v)
+
+                    # We run the FL strategy
+                    s = getattr(strats, sname)(
+                        **args, log=args_cli.log, log_basename=basename
+                    )
+                    print("FL strategy", sname, " num_updates ", num_updates)
+                    m = s.run()[0]
+
+                    perf_dict = evaluate_func(m, test_dls, metric)
+                    pooled_perf_dict = evaluate_func(m, [test_pooled], metric)
+                    print("Per-center performance:")
+                    print(perf_dict)
+                    print("Performance on pooled test set:")
+                    print(pooled_perf_dict)
+                    hyperparams_save = {
+                        k: v
+                        for k, v in hyperparameters.items()
+                        if k not in init_hp_additional_args
+                    }
+                    for k, v in perf_dict.items():
+                        perf_lines_dicts.append(
+                            prepare_dict(
+                                keys=columns_names,
+                                allow_new=True,
+                                Test=k,
+                                Metric=v,
+                                Method=sname + str(num_updates),
+                                # We add the hyperparameters used
+                                **hyperparams_save,
+                            )
+                        )
                     perf_lines_dicts.append(
                         prepare_dict(
                             keys=columns_names,
                             allow_new=True,
-                            Test=k,
-                            Metric=v,
+                            Test="Pooled Test",
+                            Metric=pooled_perf_dict["client_test_0"],
                             Method=sname + str(num_updates),
-                            # We add the hyperparameters used
+                            # We add the hyperparamters used
                             **hyperparams_save,
                         )
                     )
-                perf_lines_dicts.append(
-                    prepare_dict(
-                        keys=columns_names,
-                        allow_new=True,
-                        Test="Pooled Test",
-                        Metric=pooled_perf_dict["client_test_0"],
-                        Method=sname + str(num_updates),
-                        # We add the hyperparamters used
-                        **hyperparams_save,
-                    )
-                )
 
-                # We update csv and save it when the results are there
-                df = pd.DataFrame.from_dict(perf_lines_dicts)
-                df.to_csv(results_file, index=False)
+                    # We update csv and save it when the results are there
+                    df = pd.DataFrame.from_dict(perf_lines_dicts)
+                    df.to_csv(results_file, index=False)
 
 
 def init_data_loaders(
@@ -655,10 +691,26 @@ if __name__ == "__main__":
     parser.add_argument(
         "--results-file-path",
         "-rfp",
-        default="./results.csv",
+        default=None,
         type=str,
         help="The path to the created results (overwrite the config path)",
     )
+    parser.add_argument(
+        "--single-centric-baseline",
+        "-scb",
+        default=None,
+        type=str,
+        help="Whether or not to compute only one single-centric baseline and which one.",
+        choices=["Pooled", "Local"],
+    )
+    parser.add_argument(
+        "--nlocal",
+        default=0,
+        type=int,
+        help="Will only be used if --single-centric-baseline Local, will test"
+        "only training on Local {nlocal}.",
+    )
+
     args = parser.parse_args()
 
     main(args)
