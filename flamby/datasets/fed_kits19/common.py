5cbd57abe9a1667d5c50d2065f652e8923a69aaf
jeandut <jean.du-terrail@owkin.com>
2022-06-01 09:28:08 +0200

    Update common.py

--- flamby/datasets/fed_kits19/common.py
+++ flamby/datasets/fed_kits19/common.py
@@ -2,7 +2,7 @@ import torch
 
 NUM_CLIENTS = 6
 BATCH_SIZE = 2
-NUM_EPOCHS_POOLED = 3000
+NUM_EPOCHS_POOLED = 500 #8000 gives better performance but is too long
 LR = 3e-4
 SEEDS = [0]
 Optimizer = torch.optim.Adam
