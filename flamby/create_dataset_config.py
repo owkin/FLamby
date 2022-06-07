8d7c514561dc49aa5b14990f5668b6077255bfd2
jeandut <jean.du-terrail@owkin.com>
2022-06-07 16:20:02 +0000

    adding helper functiion

---
+++ flamby/create_dataset_config.py
@@ -0,0 +1,23 @@
+import argparse
+
+from flamby.utils import create_config, get_config_file_path, write_value_in_config
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--path",
+        type=str,
+        help="The path where the dataset is located",
+        required=True,
+    )
+    parser.add_argument("--dataset-name", type=str, help="The name of the dataset you downloaded", required=True)
+    parser.add_argument(
+        "--debug",
+        action="store_true",
+        help="whether or not to update the config fro debug mode or the real one.",
+    )
+    args = parser.parse_args()
+    dict, config_file = create_config(args.path, args.debug, args.dataset_name)
+    write_value_in_config(config_file, "dataset_path", args.path)
+    write_value_in_config(config_file, "download_complete", True)
+    write_value_in_config(config_file, "preprocessing", True)
