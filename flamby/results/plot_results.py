658bfbdbe66832dccb72cc3c70080f23d3753049
regloeb <rloeb@regisflamby.c.ruche-federated.internal>
2022-06-09 10:01:22 +0000

    first commit for benchmark results plot

--- flamby/results/plot_results.py
+++ flamby/results/plot_results.py
@@ -46,9 +46,20 @@ dir_path = os.path.dirname(os.path.realpath(__file__))
 #     df = pd.DataFrame({"Method": methods, "Metric": metrics, "Test": tests})
 #     df.to_csv(os.path.join(dir_path, f"results_benchmark_{name}.csv"), index=False)
 
-csv_files = glob(os.path.join(dir_path, "results_*.csv"))
+# ugly but needed to fix the order of the plots quickly
+# csv_files = glob(os.path.join(dir_path, "results_*.csv"))
+csv_files = [
+            os.path.join(dir_path, "results_benchmark_fed_camelyon16.csv"),
+            os.path.join(dir_path, "results_benchmark_fed_lidc_idri.csv"),
+            os.path.join(dir_path, "results_benchmark_fed_ixi.csv"),
+            os.path.join(dir_path, "results_benchmark_fed_tcga_brca.csv"),
+            os.path.join(dir_path, "results_benchmark_fed_kits19.csv"),
+            os.path.join(dir_path, "results_benchmark_fed_isic2019.csv"),
+            os.path.join(dir_path, "results_benchmark_fed_heart_disease.csv"),
+            ]
 
 # Assumes the csv file name follows results_benchmark_fed_my_dataset convention will probably break for IXITiny make sure filename has fed_ixi
+# For lidc, filename should be fed_lidc_idri
 dataset_names = [
     "_".join(csvf.split("/")[-1].split(".")[0].split("_")[2:]) for csvf in csv_files
 ]
@@ -57,10 +68,12 @@ results = [pd.read_csv(csvf) for csvf in csv_files]
 
 fig, axs = plt.subplots(2, 4, sharey=True, figsize=(40, 13), dpi=80)
 # Keep Room for Strategy labels
-fig.subplots_adjust(hspace=0.5)
+#fig.subplots_adjust(hspace=0.5)
+fig.subplots_adjust(hspace=5.0)
 flattened_axs = axs.flatten()
 
-palette = sns.color_palette("hls", 14)
+#palette = sns.color_palette("hls", 14)
+palette = sns.color_palette("mako")
 for idx, (ax, res, name) in enumerate(zip(flattened_axs, results, dataset_names)):
     # Remove pooled results
     res = res.loc[res["Test"] != "Pooled Test"]
@@ -86,8 +99,8 @@ for idx, (ax, res, name) in enumerate(zip(flattened_axs, results, dataset_names)
                     dictdf.append({"Method": c, "Metric": np.nan})
                 res = pd.DataFrame.from_dict(dictdf)
 
-    for m in current_methods:
-        assert len(res.loc[res["Method"] == m].index) == current_num_clients
+    #for m in current_methods:
+    #    assert len(res.loc[res["Method"] == m].index) == current_num_clients
 
     # Pretiifying render
     res = res.rename(columns={"Method": "Training Method"})
@@ -103,7 +116,8 @@ for idx, (ax, res, name) in enumerate(zip(flattened_axs, results, dataset_names)
         data=res,
         order=current_methods_display,
         capsize=0.05,
-        saturation=8,
+        #saturation=8,
+        saturation=2,
         errcolor="gray",
         errwidth=2,
         palette=current_palette,
@@ -118,11 +132,11 @@ for idx, (ax, res, name) in enumerate(zip(flattened_axs, results, dataset_names)
 
     ax.yaxis.set_major_locator(mticker.FixedLocator(ticks_loc))
     label_format = "{:.1f}"
-    ax.set_yticklabels([label_format.format(y) for y in ticks_loc], fontsize=35)
+    ax.set_yticklabels([label_format.format(y) for y in ticks_loc], fontsize=25)
 
-    ax.set_xticklabels(labels, rotation=90, fontsize=35, fontweight="heavy")
-    ax.set_xlabel(ax.get_xlabel(), fontsize=35, fontweight="heavy")
-    ax.set_ylabel(ax.get_ylabel(), fontsize=35, fontweight="heavy")
+    ax.set_xticklabels(labels, rotation=90, fontsize=25, fontweight="heavy")
+    ax.set_xlabel(ax.get_xlabel(), fontsize=25, fontweight="heavy")
+    ax.set_ylabel(ax.get_ylabel(), fontsize=25, fontweight="heavy")
 
     # We only display the xlabel on figures from the second row except the 4th one because it has no counterpart(label can be removed entirely)
     # and y label on the first figure of each row
@@ -130,11 +144,23 @@ for idx, (ax, res, name) in enumerate(zip(flattened_axs, results, dataset_names)
         ax.set(xlabel=None)
     if idx not in [0, 4]:
         ax.set(ylabel=None)
-    current_title = " ".join([word.capitalize() for word in name.split("_")])
 
+    # ugly but no time
+    #current_title = " ".join([word.capitalize() for word in name.split("_")])
+    title_dicts = {
+                'fed_camelyon16': 'Fed-Camelyon16',
+                'fed_lidc_idri': 'Fed-LIDC-IDRI',
+                'fed_ixi': 'Fed-IXI',
+                'fed_tcga_brca': 'Fed-TCGA-BRCA',
+                'fed_kits19': 'Fed-KITS2019',
+                'fed_isic2019': 'Fed-ISIC2019',
+                'fed_heart_disease': 'Fed-Heart-Disease',
+                }
+    current_title = title_dicts[name]
     ax.set_title(current_title, fontsize=35, fontweight="heavy")
 
 # Hide the last plot
 flattened_axs[-1].set_visible(False)
 plt.tight_layout()
 plt.savefig("plot_results_benchmarks.png")
+plt.savefig("plot_results_benchmarks.eps")
