2d3306867b51920276054b2ee067f6d5cb47b71d
jeandut <jean.du-terrail@owkin.com>
2022-06-14 13:54:50 +0200

    fixing model and benchmark script

--- flamby/datasets/fed_heart_disease/model.py
+++ flamby/datasets/fed_heart_disease/model.py
@@ -1,8 +1,9 @@
 import torch
 import torch.nn as nn
 
+
 class Baseline(nn.Module):
-    def __init__(self, input_dim=16, output_dim=1):
+    def __init__(self, input_dim=13, output_dim=1):
         super(Baseline, self).__init__()
         self.linear = torch.nn.Linear(input_dim, output_dim)
 
