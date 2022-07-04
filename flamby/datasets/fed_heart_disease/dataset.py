466e75a996f98c79743d8594c45a7eed06e75602
jeandut <jean.du-terrail@owkin.com>
2022-07-04 09:22:11 +0200

    Update flamby/datasets/fed_heart_disease/dataset.py

--- flamby/datasets/fed_heart_disease/dataset.py
+++ flamby/datasets/fed_heart_disease/dataset.py
@@ -167,7 +167,7 @@ class FedHeartDisease(HeartDiseaseRaw):
         Whether or not to use only the part of the dataset downloaded in
         debug mode. Defaults to False.
     data_path: str
-        If data_path is given it wil ignore the config file and look for the
+        If data_path is given it will ignore the config file and look for the
         dataset directly in data_path. Defaults to None.
     """
 
