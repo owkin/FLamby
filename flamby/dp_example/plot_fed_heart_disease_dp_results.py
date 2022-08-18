cd33bd42543d738d4ec3628281a38e33df96d532
jeandut <jean.du-terrail@owkin.com>
2022-08-18 09:53:47 +0200

    fixing potting

--- flamby/dp_example/plot_fed_heart_disease_dp_results.py
+++ flamby/dp_example/plot_fed_heart_disease_dp_results.py
@@ -1,6 +1,5 @@
 # Plot
 import matplotlib.pyplot as plt
-import numpy as np
 import pandas as pd
 import seaborn as sns
 
@@ -8,7 +7,12 @@ sns.set_theme(style="darkgrid")
 
 
 results = pd.read_csv("results_fed_heart_disease.csv")
-
+linestyle_str = [
+    ("solid", "solid"),  # Same as (0, ()) or '-'
+    ("dotted", "dotted"),  # Same as (0, (1, 1)) or ':'
+    ("dashed", "dashed"),  # Same as '--'
+    ("dashdot", "dashdot"),
+]
 linestyle_tuple = [
     ("loosely dotted", (0, (1, 10))),
     ("dotted", (0, (1, 1))),
@@ -23,18 +27,27 @@ linestyle_tuple = [
     ("loosely dashdotdotted", (0, (3, 10, 1, 10, 1, 10))),
     ("densely dashdotdotted", (0, (3, 1, 1, 1, 1, 1))),
 ]
-
 deltas = [d for d in results["d"].unique() if d is not None]
+fig, ax = plt.subplots()
 for i, d in enumerate(deltas):
     cdf = results.loc[results["d"] == d]
-    fig = sns.lineplot(
-        data=cdf, x="e", y="perf", label=f"delta={d}", linestyle=linestyle_tuple[i][1]
+    sns.lineplot(
+        data=cdf,
+        x="e",
+        y="perf",
+        label=f"delta={d}",
+        linestyle=linestyle_str[::-1][i][1],
+        ax=ax,
     )
-    fig.set(xscale="log")
-fig.axhline(
-    np.array(results.loc[results[d].isnull(), "d"].tolist()).mean(), color="black"
+ax.set_xscale("log")
+ax.set_xticks(
+    [d for d in results["e"].unique() if d is not None],
+    labels=[str(d) for d in results["e"].unique() if d is not None],
 )
+# fig.axhline(
+#     np.array(results.loc[results[d].isnull(), "d"].tolist()).mean(), color="black"
+# )
 plt.legend()
 plt.xlabel("epsilon")
 plt.ylabel("Perf")
-plt.savefig("trial.png")
+plt.savefig("trial.png", dpi=100, bbox_inches="tight")
