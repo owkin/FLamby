17e055903c62edc14b3c70fb0445f85a1bb67498
ThibaultFy <50656860+ThibaultFy@users.noreply.github.com>
2022-10-07 13:41:48 +0200

    feat: substra example (#219)
    * squash all
    
    Signed-off-by: ThibaultFy <50656860+ThibaultFy@users.noreply.github.com>
    
    * #substrafirst
    
    Signed-off-by: ThibaultFy <50656860+ThibaultFy@users.noreply.github.com>
    
    * #substralast
    
    Signed-off-by: ThibaultFy <50656860+ThibaultFy@users.noreply.github.com>
    
    * update plot
    
    Signed-off-by: ThibaultFy <50656860+ThibaultFy@users.noreply.github.com>
    
    * update plot
    
    Signed-off-by: ThibaultFy <50656860+ThibaultFy@users.noreply.github.com>
    
    * update local_predict
    
    Signed-off-by: ThibaultFy <50656860+ThibaultFy@users.noreply.github.com>
    
    * add download model
    
    Signed-off-by: ThibaultFy <50656860+ThibaultFy@users.noreply.github.com>
    
    * Update integration/Substra/flamby-integration-into-substra.md
    
    * Update integration/Substra/flamby-integration-into-substra.md
    
    Signed-off-by: ThibaultFy <50656860+ThibaultFy@users.noreply.github.com>
    Co-authored-by: jeandut <jean.du-terrail@owkin.com>

---
+++ integration/Substra/assets/dataset/opener_train_MyOrg2MSP.py
@@ -0,0 +1,16 @@
+import substratools as tools
+
+
+class FlambyTrainOpener(tools.Opener):
+    def get_X(self, folders):
+        config = {"center": 1, "train": True}
+        return config
+
+    def get_y(self, folders):
+        pass
+
+    def fake_X(self, n_samples=None):
+        pass
+
+    def fake_y(self, n_samples=None):
+        pass
---
+++ integration/Substra/assets/metric/metrics.py
@@ -0,0 +1,19 @@
+import substratools as tools
+from flamby.datasets.fed_tcga_brca import metric
+import numpy as np
+
+
+class TCGABRCAMetrics(tools.Metrics):
+    def score(self, inputs, outputs):
+        y_true = inputs["y"]
+        y_pred = self.load_predictions(inputs["predictions"])
+
+        perf = float(metric(y_true, y_pred))
+        tools.save_performance(perf, outputs["performance"])
+
+    def load_predictions(self, path):
+        return np.load(path)
+
+
+if __name__ == "__main__":
+    tools.metrics.execute(TCGABRCAMetrics())
