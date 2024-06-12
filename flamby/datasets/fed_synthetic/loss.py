c7478b9b66d2917fbff572df032d821bb43d20f3
jeandut <jean.du-terrail@owkin.com>
2024-06-12 11:15:27 +0200

    fix edge case (#305)

--- flamby/datasets/fed_synthetic/loss.py
+++ flamby/datasets/fed_synthetic/loss.py
@@ -8,4 +8,4 @@ class BaselineLoss(_Loss):
         self.ce = torch.nn.CrossEntropyLoss()
 
     def forward(self, input: torch.Tensor, target: torch.Tensor):
-        return self.ce(input, target.squeeze().long())
+        return self.ce(input, target.squeeze(axis=1).long())
