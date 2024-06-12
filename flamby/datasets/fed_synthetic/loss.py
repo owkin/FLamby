87994946ca26287a1d1ddfd740fa0659dbd9f6bf
jeandut <jean.du-terrail@owkin.com>
2024-06-12 10:26:17 +0200

    Update loss.py (#304)
    * Update loss.py
    
    * fixing default
    
    * trying to have good default values
    
    * fixing stuff
    
    * removing useless logic

--- flamby/datasets/fed_synthetic/loss.py
+++ flamby/datasets/fed_synthetic/loss.py
@@ -5,7 +5,7 @@ from torch.nn.modules.loss import _Loss
 class BaselineLoss(_Loss):
     def __init__(self):
         super(BaselineLoss, self).__init__()
-        self.bce = torch.nn.BCELoss()
+        self.ce = torch.nn.CrossEntropyLoss()
 
     def forward(self, input: torch.Tensor, target: torch.Tensor):
-        return self.bce(input, target)
+        return self.ce(input, target.squeeze().long())
--- flamby/datasets/fed_synthetic/metric.py
+++ flamby/datasets/fed_synthetic/metric.py
@@ -1,14 +1,9 @@
 import numpy as np
+from sklearn.metrics import accuracy_score
 
 
 def metric(y_true, y_pred):
     y_true = y_true.astype("uint8")
-    # The try except is needed because when the metric is batched some batches
-    # have one class only
-    try:
-        # return roc_auc_score(y_true, y_pred)
-        # proposed modification in order to get a metric that calcs on center 2
-        # (y=1 only on that center)
-        return ((y_pred > 0.5) == y_true).mean()
-    except ValueError:
-        return np.nan
+    y_pred = np.argmax(y_pred, axis=1)
+    return accuracy_score(y_true, y_pred)
+
