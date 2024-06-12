e4f6988e3229ae6ecfc21d8ab01d0cd559e68699
jeandut <jean.du-terrail@owkin.com>
2024-06-12 09:13:37 +0200

    removing useless logic

--- flamby/datasets/fed_synthetic/metric.py
+++ flamby/datasets/fed_synthetic/metric.py
@@ -5,9 +5,5 @@ from sklearn.metrics import accuracy_score
 def metric(y_true, y_pred):
     y_true = y_true.astype("uint8")
     y_pred = np.argmax(y_pred, axis=1)
-    # The try except is needed because when the metric is batched some batches
-    # have one class only
-    try:
-        return accuracy_score(y_true, y_pred)
-    except ValueError:
-        return np.nan
+    return accuracy_score(y_true, y_pred)
+
