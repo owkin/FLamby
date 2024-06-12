3ec19e3b715d4c93c8dde392f829855207590fd2
jeandut <jean.du-terrail@owkin.com>
2024-06-12 08:46:29 +0200

    trying to have good default values

--- flamby/datasets/fed_synthetic/metric.py
+++ flamby/datasets/fed_synthetic/metric.py
@@ -1,14 +1,13 @@
 import numpy as np
+from sklearn.metrics import accuracy_score
 
 
 def metric(y_true, y_pred):
     y_true = y_true.astype("uint8")
+    y_pred = np.argmax(y_pred, axis=1)
     # The try except is needed because when the metric is batched some batches
     # have one class only
     try:
-        # return roc_auc_score(y_true, y_pred)
-        # proposed modification in order to get a metric that calcs on center 2
-        # (y=1 only on that center)
-        return ((y_pred > 0.5) == y_true).mean()
+        return accuracy_score(y_true, y_pred)
     except ValueError:
         return np.nan
